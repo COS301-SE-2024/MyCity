702bed533177e1b4d7bd3fb5b438a2217ca0f90d
Matthew Els <117924244+MatthewEls@users.noreply.github.com>
2024-05-26 14:40:28 +0200

    fixed linting issues

--- backend/chalicelib/issues/issues_routes.py
+++ backend/chalicelib/issues/issues_routes.py
@@ -3,6 +3,6 @@ from chalice import Blueprint
 issues_routes = Blueprint(__name__)
 
 
-@issues_routes.route('/create', methods=['POST'])
+@issues_routes.route("/create", methods=["POST"])
 def create_an_issue():
-    return {'status': 'success'}
+    return {"status": "success"}
