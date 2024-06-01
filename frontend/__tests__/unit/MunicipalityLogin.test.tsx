9d6f082f660446401d263acba07253ffc773ddf6
Tino <132175332+Tinogwanz@users.noreply.github.com>
2024-06-01 21:02:00 +0200

    configured playwright for ci

---
+++ frontend/__tests__/unit/MunicipalityLogin.test.tsx
@@ -0,0 +1,44 @@
+import CitizenLogin from "@/components/Login/CitizenLogin";
+import { fireEvent, render, screen } from "@testing-library/react";
+
+describe("CitizenLogin", () => {
+
+
+
+    it("renders an email input", () => {
+        render(<CitizenLogin />);
+        const emailInput = screen.getByLabelText("Email");
+        expect(emailInput).toHaveAttribute("type", "email");
+    });
+
+    it("renders a password input", () => {
+        render(<CitizenLogin />);
+        const passwordInput = screen.getByLabelText("Password");
+        expect(passwordInput).toHaveAttribute("type", "password");
+    });
+
+
+    it("renders a forgot password link", () => {
+        render(<CitizenLogin />);
+        const forgotPasswordLink = screen.getByText("Forgot password?");
+        expect(forgotPasswordLink).toBeInTheDocument();
+    });
+
+    it("renders a submit button", () => {
+        render(<CitizenLogin />);
+        const submitButton = screen.getByText("Submit");
+        expect(submitButton).toBeInTheDocument();
+    });
+
+
+    test("onSubmit function is called after clicking submit button", () => {
+        render(<CitizenLogin />);
+        const mockFunction = jest.fn();
+        const loginForm = screen.getByTestId("loginform");
+        loginForm.addEventListener("submit", mockFunction);
+
+        fireEvent.submit(loginForm)
+        expect(mockFunction).toHaveBeenCalledTimes(1);
+    });
+
+})
