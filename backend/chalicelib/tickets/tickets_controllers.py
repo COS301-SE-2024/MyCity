8104fd15dfdf5b4709fef4dba191b28fcb2930f4
Tino <132175332+Tinogwanz@users.noreply.github.com>
2024-06-02 23:19:39 +0200

    updated tickets.routes.py and tickets.controllers.py to fix linting errors

--- backend/chalicelib/tickets/tickets_controllers.py
+++ backend/chalicelib/tickets/tickets_controllers.py
@@ -3,33 +3,31 @@ import uuid
 import boto3
 from botocore.exceptions import ClientError
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('tickets')
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("tickets")
+
 
 def create_ticket(ticket_data):
     try:
-        ticket_id = str(uuid.uuid4()) # generating a unique ticket ID
+        ticket_id = str(uuid.uuid4())  # generating a unique ticket ID
         ticket_item = {
-            'ticket_id': ticket_id,
-            'reporter': ticket_data['reporter'],
-            'asset': ticket_data['asset'],
-            'description': ticket_data['description'],
-            'state_of_asset': ticket_data['state_of_asset'],
-            'image': ticket_data['image']
+            "ticket_id": ticket_id,
+            "reporter": ticket_data["reporter"],
+            "asset": ticket_data["asset"],
+            "description": ticket_data["description"],
+            "state_of_asset": ticket_data["state_of_asset"],
+            "image": ticket_data["image"],
             #'location': ticket_data['location']
         }
         table.put_item(Item=ticket_item)
-        return {'status': 'success', 'ticket_id': ticket_id}
+        return {"status": "success", "ticket_id": ticket_id}
     except ClientError as e:
-        return {'status': 'error', 'message': str(e)}
+        return {"status": "error", "message": str(e)}
+
 
 def delete_ticket(ticket_id):
     try:
-        table.delete_item(
-            Key={
-                'ticket_id': ticket_id
-            }
-        )
-        return {'status': 'success', 'ticket_id': ticketid}
+        table.delete_item(Key={"ticket_id": ticket_id})
+        return {"status": "success", "ticket_id": ticketid}
     except ClientError as e:
-        return {'status': 'error', 'message': str(e)}
+        return {"status": "error", "message": str(e)}
