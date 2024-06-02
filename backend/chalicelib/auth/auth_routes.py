76ac975533919ba0b577cfe5b0acf5423af0d6fb
Dindosss <andindaworking@gmail.com>
2024-06-02 03:34:02 +0200

    sign uo

--- backend/chalicelib/auth/auth_routes.py
+++ backend/chalicelib/auth/auth_routes.py
@@ -5,9 +5,14 @@ import bcrypt
 auth_routes = Blueprint(__name__)
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('users')
-municipality = dynamodb.Tbale('municipalities')
-
-@auth_routes.route("/signup/user/signup", methods=["POST"])
+municipality = dynamodb.Table('municipalities')
+#signup for municipality
+@auth_routes.route("/signup/municipality", methods=["POST"])
+def signup_municipality():
+    request = auth_routes.current_request
+    return {"status": "success"}
+ ###signup for User
+@auth_routes.route("/signup/user", methods=["POST"])
 def signup_user():
     request = auth_routes.current_request
     location = "-29.789614, 30.741924"
@@ -35,6 +40,27 @@ def signup_user():
     except:
         raise ("Invalid JSON data")
     return {"status": "success"}
+###login for users
+@auth_routes.route("/login/user", methods=["POST"])
+def login_user():
+    request = auth_routes.current_request
+    data = request.json_body
+    if(not DoesFieldExist(data.get('username'),'username')):
+        return {'Error':'Userame','Message':'Username Doesnt exist'}
+    if(not DoesFieldExist(data.get('email'),'email')):
+        return {'Error':'Email','Message':'email Doesnt exist'}
+    response = table.query(
+        KeyConditionExpression=Key(field).eq(data)
+    )
+    items = response['Item']
+    for item in items:
+        password = item.get('password')
+        if verify_password(data.get('password'),password) :
+            return {"Success" : 200, 'Message' : 'Verified'}
+        else :
+            return {'Error' : 'Password', 'Message' : 'Password was wrong'}
+    
+
 
 def DoesFieldExist(data,field):
     response = table.query(
@@ -44,11 +70,16 @@ def DoesFieldExist(data,field):
         return True
     else:
         return False
+    
 def DoesMunicipalityExist(data):
     response = municipality.query(
          KeyConditionExpression=Key('municipality_id').eq(data)
     )
+
 def hashPassword(password):
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode(), salt)
     return hashed_password
+
+def verify_password( user_password,db_password):
+    return bcrypt.checkpw(user_password.encode(), db_password)
