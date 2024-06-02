172cca2667c8a024def983d9d2b492c1bf097001
Tino <132175332+Tinogwanz@users.noreply.github.com>
2024-06-02 20:59:43 +0200

    completed create ticket page

---
+++ frontend/src/components/Faults/ReportFaultForm.tsx
@@ -0,0 +1,148 @@
+'use client'
+
+import React, { useState, FormEvent } from 'react';
+import { Input, Button, Autocomplete, AutocompleteItem, Textarea, Checkbox } from '@nextui-org/react';
+import { BadgeAlert } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+
+interface ReportFaultFormProps extends React.HTMLAttributes<HTMLElement> {
+
+}
+
+
+export default function ReportFaultForm({ className }: ReportFaultFormProps) {
+    const [fault, setFault] = useState('');
+    const [faultType, setFaultType] = useState('');
+    const [streetAddress, setStreetAddress] = useState('');
+    const [suburb, setSuburb] = useState('');
+    const [city, setCity] = useState('');
+    // const [verificationCode, setVerificationCode] = useState('123456');
+    // const [showCode, setShowCode] = useState(false);
+
+
+
+    type FaultType = {
+        id: number | string;
+        name: string;
+    };
+
+
+
+
+    const faultTypes: FaultType[] = [
+        { id: 0, name: "Pothole" },
+        { id: 1, name: "Transformer" },
+        { id: 2, name: "Sewage" },
+        { id: 2, name: "Burst Water Pipe" },
+    ];
+
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        // console.log(`Province: ${province}, Fault: ${Fault}, Verification Code: ${verificationCode}`);
+    };
+
+    return (
+        <div className={cn("", className)}>
+
+            <div className="py-8 flex flex-col items-center justify-center">
+
+                <span className="text-[2.5em] font-bold">Create a Fault Ticket</span>
+
+                <div className="px-10 w-full">
+
+                    <form onSubmit={handleSubmit} className="flex flex-col gap-y-8 pt-8">
+
+                        <Autocomplete
+                            label={<span className="font-semibold text-medium">Fault type <sup className="text-blue-500">*</sup></span>}
+                            labelPlacement="outside"
+                            placeholder="Fault Type"
+                            fullWidth
+                            defaultItems={faultTypes}
+                            disableSelectorIconRotation
+                            isClearable={false}
+                            menuTrigger={"input"}
+                            size={"lg"}
+                            type="text"
+                            autoComplete="new-fault"
+                            onChange={(event) => setFaultType(event.target.value)}
+                        >
+                            {(faultType) =>
+                                <AutocompleteItem key={faultType.id} textValue={faultType.name}>
+                                    <div className="flex gap-2 items-center">
+                                        <BadgeAlert className="flex-shrink-0 text-orange-700" size={18} />
+                                        <span className="text-small">{faultType.name}</span>
+                                    </div>
+                                </AutocompleteItem>}
+                        </Autocomplete>
+
+
+
+                        <Textarea
+                            label={<span className="font-semibold text-medium">Description</span>}
+                            labelPlacement="outside"
+                            placeholder="Add Description..."
+                        />
+
+                        <div>
+                            <span className="font-semibold text-medium">Fault type <sup className="text-blue-500">*</sup></span>
+
+                            <div className="flex flex-col gap-y-5">
+
+                                <Input
+                                    variant={"bordered"}
+                                    fullWidth
+                                    classNames={{
+                                        inputWrapper: "h-[3em]",
+                                    }}
+                                    type="text"
+                                    autoComplete="new-address"
+                                    placeholder="Street Address"
+                                    value={streetAddress} onChange={(e) => setStreetAddress(e.target.value)}
+                                />
+
+                                <Input
+                                    variant={"bordered"}
+                                    fullWidth
+                                    classNames={{
+                                        inputWrapper: "h-[3em]",
+                                    }}
+                                    type="text"
+                                    autoComplete="new-suburb"
+                                    placeholder="Suburb"
+                                    value={suburb} onChange={(e) => setSuburb(e.target.value)}
+                                />
+
+                                <Input
+                                    variant={"bordered"}
+                                    fullWidth
+                                    classNames={{
+                                        inputWrapper: "h-[3em]",
+                                    }}
+                                    type="text"
+                                    autoComplete="new-city"
+                                    placeholder="City/Town"
+                                    value={city} onChange={(e) => setCity(e.target.value)}
+                                />
+
+                            </div>
+                        </div>
+
+
+                        <div className="m-auto flex items-center">
+                            <span className="text-medium font-semibold">Use current location as Fault Location? </span>
+                            <Checkbox className="ms-2.5" size={"lg"} radius="sm"></Checkbox>
+                        </div>
+
+
+
+                        <Button className="w-28 h-11 rounded-lg m-auto bg-orange-500 text-white font-semibold" type="submit">
+                            Submit
+                        </Button>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ frontend/src/components/Faults/ReportFaultMap.tsx
@@ -0,0 +1,54 @@
+
+import { Loader } from '@googlemaps/js-api-loader';
+import { MapPin } from 'lucide-react';
+import React, { useRef } from 'react';
+import { cn } from '@/lib/utils';
+import useRunOnce from '@/hooks/useRunOnce';
+
+
+interface ReportFaultMapProps extends React.HTMLAttributes<HTMLElement> {
+
+}
+
+
+export default function ReportFaultMap({ className }: ReportFaultMapProps) {
+    const mapRef = useRef<HTMLDivElement>(null);
+    let map;
+
+    const loadMap = () => {
+        const loader = new Loader({
+            apiKey: "AIzaSyD4Sj_iQXC4UckhG-EUTxuL2zwMp_oR22Y",
+            version: "weekly",
+        });
+
+
+
+        loader.importLibrary("maps").then(async () => {
+            const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+            map = new Map(mapRef.current!, {
+                center: { lat: -34.397, lng: 150.644 },
+                zoom: 8,
+                disableDefaultUI: true
+
+            });
+        });
+    };
+
+
+    useRunOnce({
+        fn: () => {
+            console.log("Runs once per session");
+            // loadMap();
+        },
+        // Session storage key ensures that the callback only runs once per session.
+        // sessionKey: "changeMeAndFnWillRerun"
+    });
+
+    return (
+        <div className={cn("relative", className)}>
+            <MapPin className="absolute top-[50%] left-[50%] z-[9999] text-red-600" size={30} />
+            <div className="w-full h-full relative" ref={mapRef}>
+            </div>
+        </div>
+    );
+}
