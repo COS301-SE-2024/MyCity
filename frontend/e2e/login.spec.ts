1ee30c3a7e53e91272f3f484a6f1a579954f40a0
Tino <132175332+Tinogwanz@users.noreply.github.com>
2024-06-03 09:40:28 +0200

    updated e2e test for login

--- frontend/e2e/login.spec.ts
+++ frontend/e2e/login.spec.ts
@@ -1,20 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test("citizen with incorrect credentials must not be able to log in", async ({ page }) => {
+test("citizen login takes them to dashboard", async ({ page }) => {
     await page.goto("http://localhost:3000/");
 
     // click the login link
     await page.getByText("Login").click();
 
     // fill in email
-    await page.getByLabel("Email").fill("president@gov.za");
+    await page.getByLabel("Email").fill("tinogwanz12@gmail.com");
 
     // fill in password
     await page.getByLabel("Password").fill("password");
 
-    // click on submit
-    await page.getByText("Submit").click();
+    // click on login button
+    await page.getByText("Login").click();
 
     // expects page url to not have changed
-    expect(page.url()).toEqual("http://localhost:3000/login");
+    expect(page.url()).toEqual("http://localhost:3000/dashboard");
 });
