472f5a9d604e33fdb4aff0e3e678c1fab18d68ad
Tino <132175332+Tinogwanz@users.noreply.github.com>
2024-06-04 13:08:28 +0200

    delete backend/tests/integration/create_view_issue_test.py

--- backend/tests/integration/create_view_issue_test.py
+++
@@ -1,4 +0,0 @@
-# test for example that if a user reports an issue,
-# then they can view their newly created issue
-
-# this is an integration test since it tests the integration between creating an issue and viewing it
