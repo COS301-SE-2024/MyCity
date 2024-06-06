ac203c81108c099f34d72df1fd13d362332a8398
Tino <132175332+Tinogwanz@users.noreply.github.com>
2024-06-06 11:15:56 +0200

    added menu dropdown

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from '@nextui-org/react';
 import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
 
@@ -30,23 +31,36 @@ export default function Navbar() {
             Login <ArrowRight className="ms-2" size={18} />
           </span>
         </Link>
-        {/* 
-        <Link href="/editprofile">
-          <span className="text-white hover:text-gray-300 cursor-pointer">
-            Edit Profile
-          </span>
-        </Link>
 
-        <Link href="/report-fault">
-          <span className="text-white hover:text-gray-300 cursor-pointer">
-            Report a Fault
-          </span>
-        </Link>
-        <Link href="/login">
-          <span className="bg-gray-500 text-white px-4 py-2 rounded-3xl cursor-pointer hover:bg-blue-600 transition duration-300 font-bold">
-            Login
-          </span>
-        </Link> */}
+
+
+
+
+
+        <Dropdown>
+          <DropdownTrigger className="cursor-pointer">
+            <Avatar showFallback isBordered className="w-[3.2rem] h-[3.2rem]" src="https://i.pravatar.cc/150?u=a04258114e29026302d" />
+
+          </DropdownTrigger>
+
+          <DropdownMenu aria-label="Menu Actions" className="px-0 py-5 gap-0 rounded-none">
+            <DropdownItem key="new" className="h-10">
+              <span className="text-medium">Account</span>
+            </DropdownItem>
+            <DropdownItem key="copy" className="h-10">
+              <span className="text-medium">Dashboard</span>
+            </DropdownItem>
+            <DropdownItem key="edit" className="h-10">
+              <span className="text-medium">Help</span>
+            </DropdownItem>
+            <DropdownItem key="delete" className="h-10">
+              <span className="text-danger text-medium">Logout</span>
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+
+
+
       </div>
     </nav>
   );
