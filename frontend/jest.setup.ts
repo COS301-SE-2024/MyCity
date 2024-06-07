3d7d3a4654f77e7952bfc49bb5c1bdd626db4174
Tino <132175332+Tinogwanz@users.noreply.github.com>
2024-06-07 12:18:55 +0200

    fixed failing tests and routing errors

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -1,2 +1,6 @@
 import "@testing-library/jest-dom";
-jest.mock('next/navigation', () => jest.requireActual('next-router-mock'));
+// jest.mock('next/navigation', () => jest.requireActual('next-router-mock'));
+jest.mock('next/navigation', () => ({
+    usePathname: jest.fn(),
+    useRouter: jest.fn()
+}));
