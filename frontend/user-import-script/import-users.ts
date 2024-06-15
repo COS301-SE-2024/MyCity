64e0ecf70443547b174135d8aeabe5e8f2ca6f7a
Tino <132175332+Tinogwanz@users.noreply.github.com>
2024-06-15 13:46:18 +0200

    created skeleton for user-import script

---
+++ frontend/user-import-script/import-users.ts
@@ -0,0 +1,13 @@
+// import promptFunction from 'prompt-sync';
+
+// const prompt = promptFunction({sigint:true});
+
+// const userType = prompt("Type of users u want to import (citizen/municipality/service-provider): ");
+
+
+
+function importUsers() {
+
+
+
+}
