04fd384b18a29b2ee1701928ef9edd0cdb4d5a30
Dindosss <andindaworking@gmail.com>
2024-06-15 20:34:42 +0200

    editing create a ticket and added geolocation file

--- backend/chalicelib/tickets/tickets_controllers.py
+++ backend/chalicelib/tickets/tickets_controllers.py
@@ -1,11 +1,14 @@
 import boto3
 from botocore.exceptions import ClientError
 import uuid
+from chalicelib.tickets.geolocation import getLatLong
+from math import radians, cos, sin, asin, sqrt
 
 dynamodb = boto3.resource("dynamodb")
 tickets_table = dynamodb.Table("tickets")
 users_table = dynamodb.Table("user")
 assets_table = dynamodb.Table("asset")
+municipality_table = dynamodb.Table('municipalities')
 
 
 def generate_id():
@@ -18,42 +21,52 @@ def create_ticket(ticket_data):
         required_fields = [
             "asset",
             "description",
-            "location",
-            "province",
-            "state",
-            "username",
+            "address",
+            "user_id",
         ]
         for field in required_fields:
             if field not in ticket_data:
-                raise BadRequestError(f"Missing required field: {field}")
+                error_response = {
+                'Error': {
+                    'Code': 'IncorrectFields',
+                    'Message': f"Missing required field: {field}"
+                }
+            }
+                raise ClientError(error_response)
 
         # Ensure user exists
-        username = ticket_data["username"]
-        user_response = users_table.get_item(Key={"username": username})
-        if "Item" not in user_response:
-            raise BadRequestError(f"User with username {username} does not exist")
 
         # Ensure asset exists
         asset_id = ticket_data["asset"]
         asset_response = assets_table.get_item(Key={"asset_id": asset_id})
         if "Item" not in asset_response:
-            raise BadRequestError(f"Asset with ID {asset_id} does not exist")
+            error_response = {
+                'Error': {
+                    'Code': 'ResourceNotFoundException',
+                    'Message': f'Asset with ID {asset_id} does not exist'
+                }
+            }
+            raise ClientError(error_response)
 
         # Generate ticket ID
         ticket_id = generate_id()
+        location = getLatLong(ticket_data['address'])
+        municipality_id = findMunicipality(location)
+
 
         # Create the ticket item
         ticket_item = {
             "ticket_id": ticket_id,
             "asset": asset_id,
-            "description": ticket_data["description"],
-            "image": ticket_data.get("image", None),
-            "location": ticket_data["location"],
-            "province": ticket_data["province"],
+            "discription": ticket_data["description"],
+            "user_id" : ticket_data['user_id'],
+            "imageURL": "https://lh3.googleusercontent.com/lWTkgY7Me1FOvsOrVdWxwn4_KbL7dNfIK6Pvtp_wkg-uIhn3ZkX1KxJhsc_2NrQn9EsrFVrnL2cgsDMnVQvl=s1028",
+            "latitude": location['latitude'],
+            "longitude": location["longitude"],
+            'municipality_id' : municipality_id,
             "state": ticket_data["state"],  # do not hard code, want to extend in future
-            "upvotes": ticket_data.get("upvotes", 0),
-            "username": username,
-            "viewed": ticket_data.get("viewed", 0),
+            "upvotes": 0,
+            "viewcount": 0,
         }
 
         # Put the ticket item into the tickets table
@@ -61,9 +74,11 @@ def create_ticket(ticket_data):
         return {"message": "Ticket created successfully", "ticket_id": ticket_id}
 
     except ClientError as e:
-        raise BadRequestError(
-            f"Failed to create ticket: {e.response['Error']['Message']}"
-        )
+        error_message = e.response['Error']['Message']
+        return {
+           'Status' : 'FAILED',
+           'Error' : error_message
+        }
 
 
 def get_fault_types():
@@ -89,6 +104,37 @@ def get_fault_types():
         return fault_types
 
     except ClientError as e:
-        raise BadRequestError(
-            f"Failed to fetch fault types: {e.response['Error']['Message']}"
-        )
+       error_message = e.response['Error']['Message']
+       return {
+           'Status' : 'FAILED',
+           'Error' : error_message
+       }
+
+
+
+def findMunicipality(location):
+    response = municipality_table.scan(ProjectionExpression = 'latitude,longitude,municipality_id')
+    latitude = radians(location['latitude'])
+    longitude = radians(location['latitude'])
+    data = response['Items']
+    closestdistance = 10000000
+    municipality = ''
+    if(len(data) > 0):
+        for x in data:
+            lat2 = x['latitude']
+            long2 = x['longitude']
+            dlat = lat2-latitude
+            dlong = long2-longitude
+            a = sin(dlat / 2)**2 + cos(latitude) * cos(lat2) * sin(dlong / 2)**2
+            r_earth = 6371
+            distance = 2*r_earth*asin(sqrt(a))
+            if distance < closestdistance:
+                closestdistance = distance
+                municipality = x['municipality_id']
+        
+        return municipality
+    else :
+        return 'No data was produced'
+
+
+
