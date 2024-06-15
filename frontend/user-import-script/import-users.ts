0ee3f8200f774f65b7ac52b1e3d2ed4f8d55165e
Tino <132175332+Tinogwanz@users.noreply.github.com>
2024-06-15 14:15:53 +0200

    added aws-amplify dependency to signup new users

--- frontend/user-import-script/import-users.ts
+++ frontend/user-import-script/import-users.ts
@@ -4,10 +4,20 @@
 
 // const userType = prompt("Type of users u want to import (citizen/municipality/service-provider): ");
 
+import { signUp } from "aws-amplify/auth";
 
 
-function importUsers() {
-
-
+async function importUsers() {
+  
+  const { isSignUpComplete, userId, nextStep } = await signUp({
+    username: "hello@mycompany.com",
+    password: "hunter2",
+    options: {
+      userAttributes: {
+        email: "hello@mycompany.com",
+        phone_number: "+15555555555" // E.164 number convention
+      },
+    }
+  });
 
 }
