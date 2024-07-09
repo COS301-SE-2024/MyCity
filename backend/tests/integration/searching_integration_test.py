a62a445e85a88611f7ff71dc4b1394d0179f0a04
Dominique-Da-Silva <u21629944@tuks.co.za>
2024-07-09 15:16:10 +0200

    small update

--- backend/tests/integration/searching_integration_test.py
+++ backend/tests/integration/searching_integration_test.py
@@ -13,18 +13,18 @@ def test_client():
 
 # Integration test for /municipality endpoint
 def test_search_municipalities_route(test_client):
-    response = test_client.http.get("/municipality?q=Ma")
+    response = test_client.http.get("/search/municipality?q=Ma")
     assert response.status_code == 200
     assert isinstance(response.json_body, list)
 
 # Integration test for /municipality-tickets endpoint
 def test_search_municipality_tickets_route(test_client):
-    response = test_client.http.get("/municipality-tickets?q=Makana%20Local")
+    response = test_client.http.get("/search/municipality-tickets?q=Makana%20Local")
     assert response.status_code == 200
     assert isinstance(response.json_body, list)
 
 # Integration test for /service-provider endpoint
 def test_search_service_providers_route(test_client):
-    response = test_client.http.get("/service-provider?q=City")
+    response = test_client.http.get("/search/service-provider?q=City")
     assert response.status_code == 200
     assert isinstance(response.json_body, list)
