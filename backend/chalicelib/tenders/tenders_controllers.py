9007b596990f85fe00fdc1ba960bd907bc6fb937
Dindosss <andindaworking@gmail.com>
2024-07-16 15:24:32 +0200

    backend

--- backend/chalicelib/tenders/tenders_controllers.py
+++ backend/chalicelib/tenders/tenders_controllers.py
@@ -8,16 +8,21 @@ from boto3.dynamodb.conditions import Attr
 import re
 import json
 from decimal import Decimal
+from datetime import datetime
+from chalicelib.tickets.tickets_controllers import generate_id
+
+dynamodb = boto3.resource("dynamodb")
+tenders_table = dynamodb.Table("tenders")
+companies_table = dynamodb.Table("private_companies")
 
 def create_tender(sender_data):
     try:
         required_fields = [
-            "AuthCode",
+            "authCode",
             "quote",
             "ticket_id",
-            "longitude",
-            "state",
-            "username",
+            "duration",
+            "price"
         ]
 
         for field in required_fields:
@@ -29,6 +34,44 @@ def create_tender(sender_data):
                     }
                 }
                 raise ClientError(error_response, "InvalideFields")
+        
+        response_company = companies_table.scan(
+            FilterExpression=Attr("authCode").eq(sender_data['authCode']),
+            ProjectionExpression="pid",
+        )
+        company_items = response_company['Items']
+        if(len(company_items)<=0):    #To see that company does exist
+            error_response = {
+                    "Error": {
+                        "Code": "CompanyDoesntExist",
+                        "Message": "Company Does not Exist",
+                    }
+                }
+            raise ClientError(error_response, "CompanyDoesntExist") 
+        
+        company_id = company_items['pid']
+        current_time = datetime.now()
+        estimated_time = Decimal(sender_data['duration']) * 24
+
+        # Format the time in the desired format
+        submitted_time = current_time.strftime("%Y-%m-%dT%H:%M:%S")
+        quote = Decimal(sender_data['price'])
+        tender_id = generate_id()
+        tender_item = {
+            "tender_id" : tender_id,
+            "company_id" : company_id,
+            "datetimefinalised" : "<empty>",
+            "datetimereviewed" : "<empty",
+            "datetimesubmitted" : submitted_time,
+            "estimatedTimeHours" : estimated_time,
+            "quote" : quote,
+            "status" : "submitted",
+            "ticket_id" : sender_data['ticket_id']
+        }
+
+        tenders_table.put_item(Item=tender_item)
+        accresponse = {"Status":"Success","message": "Tender created successfully", "tender_id": tender_id}
+        return accresponse
 
     except ClientError as e:
         error_message = e.response["Error"]["Message"]
