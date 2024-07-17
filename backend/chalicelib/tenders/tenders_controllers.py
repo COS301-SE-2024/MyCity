1b44ce7cb1bf5c9f7af6feb1541d2b2983f6da5c
Dindosss <andindaworking@gmail.com>
2024-07-17 11:47:59 +0200

    finally working

--- backend/chalicelib/tenders/tenders_controllers.py
+++ backend/chalicelib/tenders/tenders_controllers.py
@@ -15,6 +15,7 @@ dynamodb = boto3.resource("dynamodb")
 tenders_table = dynamodb.Table("tenders")
 companies_table = dynamodb.Table("private_companies")
 
+
 def create_tender(sender_data):
     try:
         required_fields = [
@@ -33,50 +34,67 @@ def create_tender(sender_data):
                     }
                 }
                 raise ClientError(error_response, "InvalideFields")
-        
-        company_items = getCompanyID(sender_data['authCode'])
-        if(len(company_items)<=0):    #To see that company does exist
+
+        company_items = getCompanyID(sender_data["authCode"])
+        if len(company_items) <= 0:  # To see that company does exist
             error_response = {
-                    "Error": {
-                        "Code": "CompanyDoesntExist",
-                        "Message": "Company Does not Exist",
-                    }
+                "Error": {
+                    "Code": "CompanyDoesntExist",
+                    "Message": "Company Does not Exist",
                 }
-            raise ClientError(error_response, "CompanyDoesntExist") 
-        
-        company_id = company_items[0]['pid']
+            }
+            raise ClientError(error_response, "CompanyDoesntExist")
+
+        company_id = company_items[0]["pid"]
+        response_check = tenders_table.scan(
+            FilterExpression=Attr("company_id").eq(company_id)
+            & Attr("ticket_id").eq(sender_data["ticket_id"]),
+        )
+
+        if len(response_check["Items"]) > 0:
+            error_response = {
+                "Error": {
+                    "Code": "TenderExist",
+                    "Message": "Company already has a tender on this Ticket",
+                }
+            }
+            raise ClientError(error_response, "TenderExist")
+
         current_time = datetime.now()
-        estimated_time = Decimal(sender_data['duration']) * 24
+        estimated_time = Decimal(sender_data["duration"]) * 24
 
         # Format the time in the desired format
         submitted_time = current_time.strftime("%Y-%m-%dT%H:%M:%S")
-        quote = Decimal(sender_data['quote'])
+        quote = Decimal(sender_data["quote"])
         tender_id = generate_id()
         tender_item = {
-            "tender_id" : tender_id,
-            "company_id" : company_id,
-            "datetimefinalised" : "<empty>",
-            "datetimereviewed" : "<empty",
-            "datetimesubmitted" : submitted_time,
-            "estimatedTimeHours" : estimated_time,
-            "quote" : quote,
-            "status" : "submitted",
-            "ticket_id" : sender_data['ticket_id']
+            "tender_id": tender_id,
+            "company_id": company_id,
+            "datetimefinalised": "<empty>",
+            "datetimereviewed": "<empty",
+            "datetimesubmitted": submitted_time,
+            "estimatedTimeHours": estimated_time,
+            "quote": quote,
+            "status": "submitted",
+            "ticket_id": sender_data["ticket_id"],
         }
 
         tenders_table.put_item(Item=tender_item)
-        accresponse = {"Status":"Success","message": "Tender created successfully", "tender_id": tender_id}
+        accresponse = {
+            "Status": "Success",
+            "message": "Tender created successfully",
+            "tender_id": tender_id,
+        }
         return accresponse
 
     except ClientError as e:
         error_message = e.response["Error"]["Message"]
         return {"Status": "FAILED", "Error": error_message}
-    
+
+
 def inreview(sender_data):
     try:
-        required_fields = [
-            "authCode",
-        ]
+        required_fields = ["authCode", "ticket_id"]
 
         for field in required_fields:
             if field not in sender_data:
@@ -87,59 +105,57 @@ def inreview(sender_data):
                     }
                 }
                 raise ClientError(error_response, "InvalideFields")
-        company_items = getCompanyID(sender_data['authCode'])
-        if(len(company_items)<=0):    #To see that company does exist
+        company_items = getCompanyID(sender_data["authCode"])
+        if len(company_items) <= 0:  # To see that company does exist
             error_response = {
-                    "Error": {
-                        "Code": "CompanyDoesntExist",
-                        "Message": "Company Does not Exist",
-                    }
+                "Error": {
+                    "Code": "CompanyDoesntExist",
+                    "Message": "Company Does not Exist",
                 }
-            raise ClientError(error_response, "CompanyDoesntExist") 
-        
-        company_id = company_items[0]['pid']
+            }
+            raise ClientError(error_response, "CompanyDoesntExist")
+
+        company_id = company_items[0]["pid"]
         print("Companyid: " + company_id)
         response_tender = tenders_table.scan(
-            FilterExpression=Attr("company_id").eq(company_id),
-            ProjectionExpression="tender_id",
+            FilterExpression=Attr("company_id").eq(company_id)
+            & Attr("ticket_id").eq(sender_data["ticket_id"]),
         )
-        tender_items = response_tender['Items']
-        print(tender_items)
-        if(len(tender_items)<=0):    #To see that company does exist
+        tender_items = response_tender["Items"]
+        if len(tender_items) <= 0:  # To see that company does exist
             error_response = {
-                    "Error": {
-                        "Code": "TenderDoesntExist",
-                        "Message": "Tender Does not Exist",
-                    }
+                "Error": {
+                    "Code": "TenderDoesntExist",
+                    "Message": "Tender Does not Exist",
                 }
-            raise ClientError(error_response, "TenderDoesntExist") 
-        tender_id = tender_items[0]['tender_id']
-        
+            }
+            raise ClientError(error_response, "TenderDoesntExist")
+        tender_id = tender_items[0]["tender_id"]
+
         current_time = datetime.now()
         reviewed_time = current_time.strftime("%Y-%m-%dT%H:%M:%S")
         updateExp = "set #status=:r, datetimereviewed=:p"
         expattrName = {"#status": "status"}
-        expattrValue = {":r": "under review",":p":reviewed_time}
-        response = updateTenderTable(tender_id,updateExp,expattrName,expattrValue)
-        if(response['ResponseMetadata']):
-            return {"Status" : "Success", "Message": response}
-        else :
+        expattrValue = {":r": "under review", ":p": reviewed_time}
+        response = updateTenderTable(tender_id, updateExp, expattrName, expattrValue)
+        if response["ResponseMetadata"]:
+            return {"Status": "Success", "Message": response}
+        else:
             error_response = {
-                    "Error": {
-                        "Code": "UpdateError",
-                        "Message": "Error occured trying to update",
-                    }
+                "Error": {
+                    "Code": "UpdateError",
+                    "Message": "Error occured trying to update",
                 }
-            raise ClientError(error_response, "UpdateError") 
+            }
+            raise ClientError(error_response, "UpdateError")
     except ClientError as e:
         error_message = e.response["Error"]["Message"]
         return {"Status": "FAILED", "Error": error_message}
-    
+
+
 def accept_tender(sender_data):
     try:
-        required_fields = [
-            "company_name",
-        ]
+        required_fields = ["company_name", "ticket_id"]
 
         for field in required_fields:
             if field not in sender_data:
@@ -150,66 +166,73 @@ def accept_tender(sender_data):
                     }
                 }
                 raise ClientError(error_response, "InvalideFields")
-            
-        company_id = getCompanIDFromName(sender_data['company_name'])
+
+        company_id = getCompanIDFromName(sender_data["company_name"])
         response_tender = tenders_table.scan(
-            FilterExpression=Attr("company_id").eq(company_id),
+            FilterExpression=Attr("company_id").eq(company_id)
+            & Attr("ticket_id").eq(sender_data["ticket_id"]),
             ProjectionExpression="tender_id",
         )
-        tender_items = response_tender['Items']
+        tender_items = response_tender["Items"]
         print(tender_items)
-        if(len(tender_items)<=0):    #To see that company does exist
+        if len(tender_items) <= 0:  # To see that company does exist
             error_response = {
-                    "Error": {
-                        "Code": "TenderDoesntExist",
-                        "Message": "Tender Does not Exist",
-                    }
+                "Error": {
+                    "Code": "TenderDoesntExist",
+                    "Message": "Tender Does not Exist",
                 }
-            raise ClientError(error_response, "TenderDoesntExist") 
-        tender_id = tender_items[0]['tender_id']
+            }
+            raise ClientError(error_response, "TenderDoesntExist")
+        tender_id = tender_items[0]["tender_id"]
         updateExp = "set #status=:r"
         expattrName = {"#status": "status"}
         expattrValue = {":r": "accepted"}
-        response = updateTenderTable(tender_id,updateExp,expattrName,expattrValue)
-        if(response['ResponseMetadata']):
-            return {"Status" : "Success", "Message": response}
-        else :
+        response = updateTenderTable(tender_id, updateExp, expattrName, expattrValue)
+        if response["ResponseMetadata"]:
+            return {"Status": "Success", "Message": response}
+        else:
             error_response = {
-                    "Error": {
-                        "Code": "UpdateError",
-                        "Message": "Error occured trying to update",
-                    }
+                "Error": {
+                    "Code": "UpdateError",
+                    "Message": "Error occured trying to update",
                 }
+            }
             raise ClientError(error_response, "UpdateError")
-
 
     except ClientError as e:
         error_message = e.response["Error"]["Message"]
-        return {"Status": "FAILED", "Error": error_message} 
-    
+        return {"Status": "FAILED", "Error": error_message}
+
 
 def getCompanyID(authcode):
     response_company = companies_table.scan(
-            FilterExpression=Attr("authCode").eq(authcode),
-            ProjectionExpression="pid",
-        )
-    return response_company['Items']
+        FilterExpression=Attr("authCode").eq(authcode),
+        ProjectionExpression="pid",
+    )
+    return response_company["Items"]
+
 
 def getCompanIDFromName(company_name):
-    response = companies_table.scan(ProjectionExpression="pid,name")
-    response_items = response['Items']
+    response = companies_table.scan()
+    response_items = response["Items"]
     company_id = ""
     for item in response_items:
-        if(company_name.lower() == item['name'].lower()):
-            company_id = item['pid']
+        if company_name.lower() == item["name"].lower():
+            company_id = item["pid"]
     return company_id
 
-def updateTenderTable(tender_id,update_expression,expression_attribute_names,expression_attribute_values):
+
+def updateTenderTable(
+    tender_id,
+    update_expression,
+    expression_attribute_names,
+    expression_attribute_values,
+):
     response = tenders_table.update_item(
-        Key={"tender_id" : tender_id},
+        Key={"tender_id": tender_id},
         UpdateExpression=update_expression,
         ExpressionAttributeNames=expression_attribute_names,
-        ExpressionAttributeValues=expression_attribute_values
+        ExpressionAttributeValues=expression_attribute_values,
     )
 
     return response
--- backend/chalicelib/tenders/tenders_routes.py
+++ backend/chalicelib/tenders/tenders_routes.py
@@ -1,28 +1,31 @@
 from chalice import Blueprint, BadRequestError, Response
-from chalicelib.tenders.tenders_controllers import(
+from chalicelib.tenders.tenders_controllers import (
     inreview,
     create_tender,
-    accept_tender
+    accept_tender,
 )
 
 tenders_blueprint = Blueprint(__name__)
 
+
 @tenders_blueprint.route("/create", methods=["POST"], cors=True)
-def create_tender_route() :
+def create_tender_route():
     request = tenders_blueprint.current_request
     sender_data = request.json_body
-    response = create_tender(sender_data) 
+    response = create_tender(sender_data)
     return response
 
+
 @tenders_blueprint.route("/in-review", methods=["POST"], cors=True)
-def in_review() :
+def in_review():
     request = tenders_blueprint.current_request
     sender_data = request.json_body
     response = inreview(sender_data)
     return response
 
+
 @tenders_blueprint.route("/accept", methods=["POST"], cors=True)
-def accepting_tenders() :
+def accepting_tenders():
     request = tenders_blueprint.current_request
     sender_data = request.json_body
     response = accept_tender(sender_data)
