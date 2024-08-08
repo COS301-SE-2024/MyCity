b2b894f83ae9d9c49d8ec76d75454e16d782623d
Dominique-Da-Silva <u21629944@tuks.co.za>
2024-08-08 13:29:01 +0200

    Changed frontend, faulty integration

--- frontend/src/app/search/citizen/page.tsx
+++ frontend/src/app/search/citizen/page.tsx
@@ -7,6 +7,7 @@ import SearchMunicipality from "@/components/Search/SearchMunicipality";
 import SearchSP from "@/components/Search/SearchSP";
 import { HelpCircle, X } from "lucide-react";
 import { ThreeDots } from "react-loader-spinner";
+import { useProfile } from "@/hooks/useProfile";
 import {
   searchIssue,
   searchMunicipality,
@@ -18,11 +19,9 @@ export default function CreateTicket() {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState<any[]>([]);
   const [selectedFilter, setSelectedFilter] = useState<
-    "myLocation" | "serviceProviders" | "municipalities" | "municipalityTickets"
-  >("myLocation");
+    "myMunicipality" | "serviceProviders" | "municipalities"
+  >("myMunicipality");
   const [hasSearched, setHasSearched] = useState(false);
-  const [municipalityTickets, setMunicipalityTickets] = useState<any[]>([]);
-  const [isHelpModalOpen, setIsHelpModalOpen] = useState(false);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [resultsPerPage] = useState(10);
@@ -30,50 +29,47 @@ export default function CreateTicket() {
   const [showToast, setShowToast] = useState(false);
   const [totalResults, setTotalResults] = useState(0);
   const [selectedSubfilter, setSelectedSubfilter] = useState(0);
+  const userProfile = useProfile();
+  const [isHelpModalOpen, setIsHelpModalOpen] = useState(false);
+
+
 
   const handleSearch = async () => {
+
+    
     try {
       setHasSearched(true);
       setLoading(true);
       const startTime = Date.now();
       let data: any[] = [];
-      let allTickets = [];
+
+      // Get user profile and municipality
+      const user_data = await userProfile.getUserProfile();
+      const user_municipality = String(user_data.current?.municipality);
+      const user_session = String(user_data.current?.session_token);
+
       switch (selectedFilter) {
-        case "myLocation":
-          data = await searchIssue(searchTerm);
+        case "myMunicipality": // My Municipality -> Near Me or Asset
+          if (selectedSubfilter === 0) {
+            data = await searchMunicipalityTickets(user_municipality);
+          } else if (selectedSubfilter === 1) {
+            data = await searchIssue(searchTerm + ` ${user_municipality}`);
+          }
           break;
-        case "serviceProviders":
+        case "serviceProviders": // Service Providers
           data = await searchServiceProvider(searchTerm);
           break;
-        case "municipalities":
+        case "municipalities": // Municipalities
           data = await searchMunicipality(searchTerm);
-          break;
-        case "municipalityTickets":
-          data = await searchMunicipality(searchTerm);
-          const municipalityIds = data.map(
-            (municipality: any) => municipality.municipality_id
-          );
-          const ticketsPromises = municipalityIds.map(
-            (municipalityId: string) =>
-              searchMunicipalityTickets(municipalityId)
-          );
-          const ticketsResponses = await Promise.all(ticketsPromises);
-          allTickets = ticketsResponses.flatMap((response) => response);
-          setMunicipalityTickets(allTickets);
           break;
         default:
           break;
       }
+
       const endTime = Date.now();
       setSearchTime((endTime - startTime) / 1000); // Time in seconds
-      setTotalResults(
-        selectedFilter === "municipalityTickets"
-          ? allTickets.length
-          : data.length
-      );
-      setSearchResults(
-        selectedFilter === "municipalityTickets" ? allTickets : data
-      );
+      setTotalResults(data.length);
+      setSearchResults(data);
       setShowToast(true);
       setTimeout(() => setShowToast(false), 3000); // Hide toast after 3 seconds
       setLoading(false);
@@ -84,17 +80,18 @@ export default function CreateTicket() {
   };
 
   const handleFilterChange = (
-    filter:
-      | "myLocation"
-      | "serviceProviders"
-      | "municipalities"
-      | "municipalityTickets"
+    filter: "myMunicipality" | "serviceProviders" | "municipalities"
   ) => {
     setSelectedFilter(filter);
+    setSelectedSubfilter(0); // Reset subfilter to default
     setSearchResults([]);
     setHasSearched(false);
-    setMunicipalityTickets([]);
     setCurrentPage(1); // Reset to the first page on filter change
+  };
+
+  const handleSubfilterChange = (index: number) => {
+    setSelectedSubfilter(index);
+    handleSearch(); // Trigger a search whenever the subfilter changes
   };
 
   const handleSearchInputChange = (
@@ -223,42 +220,36 @@ export default function CreateTicket() {
               </form>
 
               <div className="flex mt-4 relative">
-                {[
-                  "myLocation",
-                  "municipalities",
-                  "municipalityTickets",
-                  "serviceProviders",
-                ].map((filter) => (
-                  <div
-                    key={filter}
-                    className={`px-4 py-2 mx-1 cursor-pointer rounded-full transition duration-300 ${
-                      selectedFilter === filter
-                        ? "bg-gray-500 text-white"
-                        : "bg-transparent text-white"
-                    }`}
-                    onClick={() =>
-                      handleFilterChange(
-                        filter as
-                          | "myLocation"
-                          | "municipalities"
-                          | "municipalityTickets"
-                          | "serviceProviders"
-                      )
-                    }
-                  >
-                    {filter === "myLocation"
-                      ? "Current Municipality"
-                      : filter === "municipalities"
-                      ? "Municipalities"
-                      : filter === "municipalityTickets"
-                      ? "Tickets"
-                      : "Service Providers"}
-                  </div>
-                ))}
+                {["myMunicipality", "municipalities", "serviceProviders"].map(
+                  (filter) => (
+                    <div
+                      key={filter}
+                      className={`px-4 py-2 mx-1 cursor-pointer rounded-full transition duration-300 ${
+                        selectedFilter === filter
+                          ? "bg-gray-500 text-white"
+                          : "bg-transparent text-white"
+                      }`}
+                      onClick={() =>
+                        handleFilterChange(
+                          filter as
+                            | "myMunicipality"
+                            | "municipalities"
+                            | "serviceProviders"
+                        )
+                      }
+                    >
+                      {filter === "myMunicipality"
+                        ? "My Municipality"
+                        : filter === "municipalities"
+                        ? "Municipalities"
+                        : "Service Providers"}
+                    </div>
+                  )
+                )}
               </div>
-              {selectedFilter === "municipalityTickets" && (
+              {selectedFilter === "myMunicipality" && (
                 <div className="flex mt-6">
-                  {["Type", "Urgency", "Status"].map((subfilter, index) => (
+                  {["Near Me", "Asset"].map((subfilter, index) => (
                     <div
                       key={subfilter}
                       className={`px-3 py-1 mx-1 cursor-pointer rounded-full transition duration-300 ${
@@ -266,7 +257,7 @@ export default function CreateTicket() {
                           ? "bg-gray-500 text-white"
                           : "bg-transparent text-gray-300 border border-gray-300"
                       }`}
-                      onClick={() => setSelectedSubfilter(index)}
+                      onClick={() => handleSubfilterChange(index)}
                     >
                       {subfilter}
                     </div>
@@ -275,148 +266,78 @@ export default function CreateTicket() {
               )}
             </div>
 
-            {loading && (
-              <div className="flex justify-center items-center mt-8">
+            {loading ? (
+              <div className="flex justify-center items-center mt-4">
                 <ThreeDots
-                  height="40"
+                  height="80"
                   width="80"
                   radius="9"
-                  color="#ADD8E6"
+                  color="white"
                   ariaLabel="three-dots-loading"
                   wrapperStyle={{}}
-                  wrapperClass=""
                   visible={true}
                 />
               </div>
-            )}
-            {!loading && hasSearched && searchResults.length === 0 && (
+            ) : hasSearched && searchResults.length > 0 ? (
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+                {currentResults.map((result, index) => (
+                  <div
+                    key={index}
+                    className="p-4 bg-white bg-opacity-80 rounded-lg shadow-md"
+                  >
+                    {selectedFilter === "myMunicipality" && (
+                      <SearchTicket tickets={result} />
+                    )}
+                    {selectedFilter === "municipalities" && (
+                      <SearchMunicipality municipalities={result} />
+                    )}
+                    {selectedFilter === "serviceProviders" && (
+                      <SearchSP serviceProviders={result} />
+                    )}
+                  </div>
+                ))}
+              </div>
+            ) : hasSearched ? (
               <div className="flex justify-center items-center mt-4">
-                <p className="mt-16 text-white text-opacity-80">
-                  No results found. Please try a different search term.
+                <p className="text-white text-opacity-80">
+                  No results found for "{searchTerm}".
+                </p>
+              </div>
+            ) : (
+              <div className="flex justify-center items-center mt-4">
+                <p className="text-white text-opacity-80">
+                  Please enter a search term to begin.
                 </p>
               </div>
             )}
 
-            {hasSearched && !loading && (
-              <>
-                {currentResults.map((result, index) => {
-                  if (selectedFilter === "serviceProviders") {
-                    return <SearchSP key={index} serviceProviders={[result]} />;
-                  }
-                  if (selectedFilter === "municipalities") {
-                    return (
-                      <SearchMunicipality
-                        key={index}
-                        municipalities={[result]}
-                      />
-                    );
-                  }
-                  if (selectedFilter === "municipalityTickets") {
-                    return <SearchTicket key={index} tickets={[result]} />;
-                  }
-                  return null;
-                })}
-                <div className="flex justify-center mt-4">
-                  <nav>
-                    <ul className="flex list-none p-0">
-                      {Array.from(
-                        {
-                          length: Math.ceil(
-                            searchResults.length / resultsPerPage
-                          ),
-                        },
-                        (_, index) => (
-                          <li key={index} className="mx-1">
-                            <button
-                              onClick={() => paginate(index + 1)}
-                              className={`px-3 py-1 rounded-full ${
-                                currentPage === index + 1
-                                  ? "bg-blue-500 text-white"
-                                  : "bg-gray-300"
-                              }`}
-                            >
-                              {index + 1}
-                            </button>
-                          </li>
-                        )
-                      )}
-                    </ul>
-                  </nav>
-                </div>
-              </>
-            )}
-            {showToast && (
-              <div className="fixed bottom-4 left-4 bg-white text-black px-4 py-2 rounded shadow-lg">
-                <span>{totalResults} results found in </span>
-                <span className="text-blue-500">{searchTime.toFixed(2)}</span>
-                <span> seconds</span>
-              </div>
-            )}
-          </main>
-        </div>
-      </div>
-
-      {/* Mobile View */}
-      <div className="block sm:hidden">
-        <div
-          style={{
-            position: "relative",
-            height: "100vh",
-            overflow: "hidden", // Prevents content overflow
-          }}
-        >
-          <div className="text-white font-bold ms-2 transform hover:scale-105 mt-5 ml-5 transition-transform duration-200">
-            <img
-              src="https://i.imgur.com/WbMLivx.png"
-              alt="MyCity"
-              width={100}
-              height={100}
-              className="w-100 h-100"
-            />
-          </div>
-
-          {/* Background image */}
-          <div
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-              backgroundImage:
-                'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("https://www.andbeyond.com/wp-content/uploads/sites/5/Johannesburg-Skyline.jpg")',
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-              zIndex: -1, // Ensures the background is behind other content
-            }}
-          ></div>
-
-          {/* Content */}
-          <div className="h-[5vh] flex items-center justify-center"></div>
-          <div className="container mx-auto relative z-10">
-            {" "}
-            {/* Ensure content is above the background */}
-            <h1 className="text-4xl text-white font-bold mb-4 ml-4">
-              <span className="text-blue-200">MyCity</span> <br />
-              Under Construction
-            </h1>
-            <div className="text-white font-bold transform hover:scale-105 transition-transform duration-200 flex justify-center">
-              <img
-                src="https://i.imgur.com/eGeTTuo.png"
-                alt="Under-Construction"
-                width={300}
-                height={300}
-              />
+            <div className="flex justify-center mt-4">
+              {Array.from({
+                length: Math.ceil(searchResults.length / resultsPerPage),
+              }).map((_, index) => (
+                <button
+                  key={index}
+                  onClick={() => paginate(index + 1)}
+                  className={`px-3 py-1 mx-1 rounded-full transition duration-300 ${
+                    currentPage === index + 1
+                      ? "bg-gray-500 text-white"
+                      : "bg-transparent text-white"
+                  }`}
+                >
+                  {index + 1}
+                </button>
+              ))}
             </div>
-            <p className="text-lg text-gray-200 mb-4 ml-4">
-              Our Mobile site is currently under construction.
-              <br />
-              Please use our Desktop site while we
-              <br />
-              work on it.
-            </p>
-          </div>
+            <div
+              className={`fixed bottom-4 left-4 bg-green-500 text-white p-3 rounded-lg shadow-lg transition-opacity duration-300 ${
+                showToast ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              <p>
+                Found {totalResults} results in {searchTime.toFixed(2)} seconds.
+              </p>
+            </div>
+          </main>
         </div>
       </div>
     </div>
