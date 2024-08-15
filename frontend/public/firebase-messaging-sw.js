845b137714684a13db67de984762e4f5d9533d6e
Matthew <117924244+MatthewEls@users.noreply.github.com>
2024-08-16 00:19:22 +0200

    fix

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -6,13 +6,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compa
 // import { getMessaging, getToken } from "firebase/messaging";
 
 const firebaseConfig = {
-apikey: String(process.env.NEXT_PUBLIC_FIREBASE_API_KEY),
-authDomain: String(process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN),
-projectId: String(process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID),
-storageBucket: String(process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET),
-messagingSenderId: String(process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
-appId: String(process.env.NEXT_PUBLIC_FIREBASE_APP_ID),
-measurementId: String(process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID)
+apikey: String(process.env.FIREBASE_API_KEY),
+authDomain: String(process.env.FIREBASE_AUTH_DOMAIN),
+projectId: String(process.env.FIREBASE_PROJECT_ID),
+storageBucket: String(process.env.FIREBASE_STORAGE_BUCKET),
+messagingSenderId: String(process.env.FIREBASE_MESSAGING_SENDER_ID),
+appId: String(process.env.FIREBASE_APP_ID),
+measurementId: String(process.env.FIREBASE_MEASUREMENT_ID)
 };
 
 firebase.initializeApp(firebaseConfig);
--- frontend/src/components/Notifications/firebase.ts
+++ frontend/src/components/Notifications/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, FirebaseApp } from "firebase/app";
 import { getMessaging, Messaging, onMessage, getToken } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -37,7 +37,7 @@ export const generateToken = async () => {
   if (permission === 'granted') {
     try {
       const token = await getToken(messaging, {
-        vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY || ""
+        vapidKey: process.env.FIREBASE_VAPID_KEY || ""
       });
       console.log("Token generated: ", token);
     } catch (error) {
