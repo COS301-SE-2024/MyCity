6452b9718bb8b89929254c0840d14e48dd934458
Matthew <117924244+MatthewEls@users.noreply.github.com>
2024-08-26 18:32:57 +0200

    converted query to getAll

--- frontend/__tests__/integration/login.test.tsx
+++ frontend/__tests__/integration/login.test.tsx
@@ -5,10 +5,12 @@ describe("Login Page", () => {
 
     it("renders CitizenLogin form by default", () => {
         render(<Login />);
-        const citizenForm = screen.queryByTestId("citizen-login-form");
-        const municipalityForm = screen.queryByTestId("municipality-login-form");
-        const serviceProviderForm = screen.queryByTestId("service-provider-login-form");
-
+        const citizenForms = screen.getAllByTestId("citizen-login-form");
+        const citizenForm = citizenForms[0];
+        const municipalityForms = screen.getAllByTestId("municipality-login-form");
+        const municipalityForm = municipalityForms[0];
+        const serviceProviderForms = screen.getAllByTestId("service-provider-login-form");
+        const serviceProviderForm = serviceProviderForms[0];
         expect(citizenForm).toBeInTheDocument();
         expect(municipalityForm).not.toBeInTheDocument();
         expect(serviceProviderForm).not.toBeInTheDocument();
@@ -32,14 +34,17 @@ describe("Login Page", () => {
 
     it("renders Service Provider form when Service Provider tab is clicked", () => {
         render(<Login />);
-        const tabButton = screen.getByTestId("service-provider-tab");
+        const tabButtons = screen.getAllByTestId("service-provider-tab");
+        const tabButton = tabButtons[0];
         fireEvent.click(tabButton);
         fireEvent.select(tabButton);
 
-        const citizenForm = screen.queryByTestId("citizen-login-form");
-        const municipalityForm = screen.queryByTestId("municipality-login-form");
-        const serviceProviderForm = screen.queryByTestId("service-provider-login-form");
-
+        const citizenForms = screen.getAllByTestId("citizen-login-form");
+        const citizenForm = citizenForms[0];
+        const municipalityForms = screen.getAllByTestId("municipality-login-form");
+        const municipalityForm = municipalityForms[0];
+        const serviceProviderForms = screen.getAllByTestId("service-provider-login-form");
+        const serviceProviderForm = serviceProviderForms[0];
         expect(municipalityForm).not.toBeInTheDocument();
         expect(citizenForm).not.toBeInTheDocument();
         expect(serviceProviderForm).toBeInTheDocument();
