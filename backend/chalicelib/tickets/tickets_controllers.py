f30832ffa5fa07c0ff824bfe55febde0f44d3e67
Dindosss <andindaworking@gmail.com>
2024-08-26 04:48:27 +0200

    reformatted and function works

--- backend/chalicelib/tickets/tickets_controllers.py
+++ backend/chalicelib/tickets/tickets_controllers.py
@@ -152,7 +152,7 @@ def create_ticket(ticket_data):
     except ClientError as e:
         error_message = e.response["Error"]["Message"]
         return {"Status": "FAILED", "Error": error_message}
-    
+
 
 def add_watchlist(ticket_data):
     try:
@@ -169,11 +169,12 @@ def add_watchlist(ticket_data):
                     }
                 }
                 raise ClientError(error_response, "InvalideFields")
-            
+
         user_exist = watchlist_table.scan(
-            FilterExpression=Attr("user_id").eq(ticket_data['username']) &  Attr("ticket_id").eq(ticket_data['ticket_id'])
+            FilterExpression=Attr("user_id").eq(ticket_data["username"])
+            & Attr("ticket_id").eq(ticket_data["ticket_id"])
         )
-        if len(user_exist['Items']) > 0:
+        if len(user_exist["Items"]) > 0:
             error_response = {
                 "Error": {
                     "Code": "AlreadyExists",
@@ -181,10 +182,8 @@ def add_watchlist(ticket_data):
                 }
             }
             raise ClientError(error_response, "AlreadyExists")
-        
-        
-        
-        if TicketExists(ticket_data['ticket_id']) == False:
+
+        if DoesTicketExist(ticket_data["ticket_id"]) == False:
             error_response = {
                 "Error": {
                     "Code": "TicketDoesntExists",
@@ -192,32 +191,27 @@ def add_watchlist(ticket_data):
                 }
             }
             raise ClientError(error_response, "TicketDoesntExists")
-        
+
         watchlist_id = generate_id()
-        
+
         watchlist_item = {
-            "watchlist_id" : watchlist_id,
-            "ticket_id" : ticket_data['ticket_id'],
-            "user_id" : ticket_data['username'],
+            "watchlist_id": watchlist_id,
+            "ticket_id": ticket_data["ticket_id"],
+            "user_id": ticket_data["username"],
         }
 
         watchlist_table.put_item(Item=watchlist_item)
-        return {"Status": "Success","Message" : "ticket has been added to " + ticket_data['username']}
-        
+        return {
+            "Status": "Success",
+            "Message": "ticket has been added to "
+            + ticket_data["username"]
+            + "with id of :"
+            + watchlist_id,
+        }
+
     except ClientError as e:
         error_message = e.response["Error"]["Message"]
         return {"Status": "FAILED", "Error": error_message}
-    
-
-def TicketExists(ticket_id):
-    exists = tickets_table.query(
-        KeyConditionExpression=Key("ticket_id").eq(ticket_id)
-    )
-    if len(exists['Items']) > 0:
-        return True
-    else:
-        return False
-
 
 
 def get_fault_types():
@@ -584,7 +578,9 @@ def ClosedTicket(ticket_data):
         current_datetime = datetime.now()
         formatted_datetime = current_datetime.strftime("%Y-%m-%dT%H:%M:%S")
         CloseexpattrValue = {":r": formatted_datetime}
-        response_closed = updateTicketTable(ticket_id, dateExpr, CloseexpattrName, CloseexpattrValue)
+        response_closed = updateTicketTable(
+            ticket_id, dateExpr, CloseexpattrName, CloseexpattrValue
+        )
         if response["ResponseMetadata"]:
             return {
                 "Status": "Success",
