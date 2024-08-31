67d24f7fe8e632e35ebd8d98390affba5d0f0f2c
Matthew <117924244+MatthewEls@users.noreply.github.com>
2024-08-31 14:20:50 +0200

    updated fault view

--- frontend/src/components/FaultCardUserView/FaultCardUserView.tsx
+++ frontend/src/components/FaultCardUserView/FaultCardUserView.tsx
@@ -1,11 +1,103 @@
 import React, { useState, useEffect } from "react";
-import { FaArrowUp, FaComment, FaEye, FaExclamationTriangle, FaTimes } from "react-icons/fa";
+import {
+  FaArrowUp,
+  FaComment,
+  FaEye,
+  FaExclamationTriangle,
+  FaTimes,
+} from "react-icons/fa";
 import MapComponent from "@/context/MapboxMap"; // Adjust the import path as necessary
 import Comments from "../Comments/comments"; // Adjust the import path as necessary
 import { Button } from "@nextui-org/react";
 import { MapPin, Image as ImageIcon } from "lucide-react"; // Added ImageIcon from lucide-react
 import { InteractTicket } from "@/services/tickets.service";
 import { useProfile } from "@/hooks/useProfile";
+import { Eye, Key } from "lucide-react";
+import { MessageCirclePlus } from "lucide-react";
+import { ArrowBigUp } from "lucide-react";
+
+interface cardDataWatchlist {
+  title: string;
+  address: string;
+  arrowCount: number;
+  commentCount: number;
+  viewCount: number;
+  ticketNumber: string;
+  description: string;
+  image: string;
+  createdBy: string;
+  state: string;
+  municipality_id: string;
+  key: string;
+  stateFormat: keyof typeof notificationStates;
+}
+
+interface StatusCardUserProps extends cardDataWatchlist {
+  onClick?: () => void; // Make onClick optional
+}
+
+// states.ts
+export const notificationStates = {
+  AssigningContract: {
+    color: "bg-blue-200",
+    text: "Assigning Contract",
+  },
+  Closed: {
+    color: "bg-green-200",
+    text: "Closed",
+  },
+  InProgress: {
+    color: "bg-yellow-200",
+    text: "In Progress",
+  },
+  Opened: {
+    color: "bg-red-200",
+    text: "Opened",
+  },
+  TakingTenders: {
+    color: "bg-purple-200",
+    text: "Taking Tenders",
+  },
+  Default: {
+    color: "bg-gray-200",
+    text: "Taking Tenders",
+  },
+};
+
+function formatMunicipalityID(mun: string): string {
+  if (typeof mun !== "string") {
+    return ""; // Or some other default value
+  }
+  return mun.replace(/ /g, "");
+}
+
+function getDate(date: string): string {
+  return date.split("T")[0];
+}
+function getTime(date: string): string {
+  return date.split("T")[1].split(".")[0];
+}
+
+function displayDate(date: string): string {
+  const inputDate = new Date(date);
+  const currentDate = new Date();
+
+  const inputDay = inputDate.toISOString().split("T")[0];
+  const currentDay = currentDate.toISOString().split("T")[0];
+
+  if (inputDay === currentDay) {
+    return getTime(date);
+  } else {
+    return getDate(date);
+  }
+}
+
+function formatState(state: string | undefined): string {
+  if (typeof state !== "string") {
+    return ""; // Or some other default value
+  }
+  return state.replace(/ /g, "");
+}
 
 interface FaultCardUserViewProps {
   show: boolean;
@@ -22,13 +114,24 @@ interface FaultCardUserViewProps {
   createdBy: string;
   latitude: number;
   longitude: number;
+  state: string;
+  municipality_id: string;
   urgency: "high" | "medium" | "low";
 }
 
 const urgencyMapping = {
-  high: { icon: <FaExclamationTriangle className="text-red-500" />, label: "Urgent" },
-  medium: { icon: <FaExclamationTriangle className="text-yellow-500" />, label: "Moderate" },
-  low: { icon: <FaExclamationTriangle className="text-green-500" />, label: "Not Urgent" },
+  high: {
+    icon: <FaExclamationTriangle className="text-red-500" />,
+    label: "Urgent",
+  },
+  medium: {
+    icon: <FaExclamationTriangle className="text-yellow-500" />,
+    label: "Moderate",
+  },
+  low: {
+    icon: <FaExclamationTriangle className="text-green-500" />,
+    label: "Not Urgent",
+  },
 };
 
 const formatNumber = (num: number) => {
@@ -50,12 +153,17 @@ const FaultCardUserView: React.FC<FaultCardUserViewProps> = ({
   viewCount,
   ticketId,
   ticketNumber,
+  state,
+  municipality_id,
   description,
   image,
   latitude,
   longitude,
-  urgency
+  urgency,
 }) => {
+  const formattedStateKey = formatState(state);
+  // const { color, text } = notificationStates[formattedStateKey];
+  const { color, text } = notificationStates["InProgress"];
   const [currentArrowCount, setCurrentArrowCount] = useState(arrowCount);
   const [currentCommentCount, setCurrentCommentCount] = useState(commentCount);
   const [currentViewCount, setCurrentViewCount] = useState(viewCount);
@@ -67,33 +175,29 @@ const FaultCardUserView: React.FC<FaultCardUserViewProps> = ({
   const [isInitialRender, setIsInitialRender] = useState(true);
   const userProfile = useProfile();
 
-
   useEffect(() => {
-   
-      const storedData = localStorage.getItem(`ticket-${ticketNumber}`);
-      if (storedData) {
-        const data = JSON.parse(storedData);
-        setCurrentArrowCount(data.arrowCount);
-        setCurrentCommentCount(data.commentCount);
-        setCurrentViewCount(data.viewCount);
-        setArrowColor(data.arrowColor);
-        setCommentColor(data.commentColor);
-        setEyeColor(data.eyeColor);
-      }
-      else 
-      {
-        const data = {
-          arrowCount: currentArrowCount,
-          commentCount: currentCommentCount,
-          viewCount: currentViewCount,
-          arrowColor,
-          commentColor,
-          eyeColor,
-        };
-    
-        localStorage.setItem(`ticket-${ticketNumber}`, JSON.stringify(data));
-      }
-  }, [ticketNumber,isInitialRender]);
+    const storedData = localStorage.getItem(`ticket-${ticketNumber}`);
+    if (storedData) {
+      const data = JSON.parse(storedData);
+      setCurrentArrowCount(data.arrowCount);
+      setCurrentCommentCount(data.commentCount);
+      setCurrentViewCount(data.viewCount);
+      setArrowColor(data.arrowColor);
+      setCommentColor(data.commentColor);
+      setEyeColor(data.eyeColor);
+    } else {
+      const data = {
+        arrowCount: currentArrowCount,
+        commentCount: currentCommentCount,
+        viewCount: currentViewCount,
+        arrowColor,
+        commentColor,
+        eyeColor,
+      };
+
+      localStorage.setItem(`ticket-${ticketNumber}`, JSON.stringify(data));
+    }
+  }, [ticketNumber, isInitialRender]);
 
   const toggleComments = () => {
     setShowComments((prev) => !prev);
@@ -102,44 +206,40 @@ const FaultCardUserView: React.FC<FaultCardUserViewProps> = ({
   const handleArrowClick = async () => {
     if (arrowColor === "black") {
       setArrowColor("blue");
-      console.log("inside arrwossss")
+      console.log("inside arrwossss");
       const user_data = await userProfile.getUserProfile();
       const userSession = String(user_data.current?.session_token);
-      const rspvotes = await InteractTicket(ticketId,"UPVOTE",userSession)
-      if(rspvotes !== -1)
-      {
-        console.log("Inside rspmovetss")
+      const rspvotes = await InteractTicket(ticketId, "UPVOTE", userSession);
+      if (rspvotes !== -1) {
+        console.log("Inside rspmovetss");
         setCurrentArrowCount(rspvotes);
         const data = {
           arrowCount: rspvotes,
           commentCount: currentCommentCount,
           viewCount: currentViewCount,
-          arrowColor : "blue",
+          arrowColor: "blue",
           commentColor,
           eyeColor,
         };
         localStorage.setItem(`ticket-${ticketNumber}`, JSON.stringify(data));
-      }
-      else setCurrentArrowCount((prevCount: any) => prevCount + 1);
+      } else setCurrentArrowCount((prevCount: any) => prevCount + 1);
     } else {
       setArrowColor("black");
       const user_data = await userProfile.getUserProfile();
       const userSession = String(user_data.current?.session_token);
-      const rspvotes = await InteractTicket(ticketId,"UNVOTE",userSession)
-      if(rspvotes !== -1)
-      {
+      const rspvotes = await InteractTicket(ticketId, "UNVOTE", userSession);
+      if (rspvotes !== -1) {
         setCurrentArrowCount(rspvotes);
         const data = {
           arrowCount: rspvotes,
           commentCount: currentCommentCount,
           viewCount: currentViewCount,
-          arrowColor : "black",
+          arrowColor: "black",
           commentColor,
           eyeColor,
         };
         localStorage.setItem(`ticket-${ticketNumber}`, JSON.stringify(data));
-      }
-      else setCurrentArrowCount((prevCount: any) => prevCount - 1);
+      } else setCurrentArrowCount((prevCount: any) => prevCount - 1);
     }
   };
 
@@ -178,7 +278,7 @@ const FaultCardUserView: React.FC<FaultCardUserViewProps> = ({
       onClick={onClose} // Close modal when clicking outside
     >
       <div
-        className="bg-white rounded-lg shadow-lg w-11/12 md:w-3/4 lg:w-3/4 xl:w-2/3 max-w-4xl max-h-[90vh] p-4 relative flex flex-col lg:flex-row"
+        className="bg-white rounded-lg shadow-lg w-2/3 max-h-[90vh] p-4 relative flex flex-col border border-red-300 justify-center"
         onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside
       >
         <button
@@ -188,96 +288,164 @@ const FaultCardUserView: React.FC<FaultCardUserViewProps> = ({
           <FaTimes size={24} />
         </button>
 
-        <div className="relative w-full lg:w-1/3 p-2 flex flex-col items-center">
-          <div className="flex items-center">
-            <h2 className="font-bold text-black text-2xl">{`Ticket`}</h2>
-          </div>
-          <div className="mt-2 mb-2 text-center">
-            <h3 className="font-bold text-black text-lg">Fault Type</h3>
-            <p className="text-gray-700">{title}</p>
-          </div>
-          <div className="mb-2 text-center">
-            <h3 className="font-bold text-black text-lg">Description</h3>
-            <p className="text-gray-700">{description}</p>
-          </div>
-          {image && !imageError ? (
-            <div className="mb-2 flex justify-center">
-              <img
-                src={image}
-                alt="Fault"
-                className="rounded-lg w-48 h-36 object-cover"
-                onError={() => setImageError(true)} // Set error state if image fails to load
-              />
+        {/* Header */}
+        <div className="flex justify-center">
+          <div className="flex w-full justify-between items-center">
+            {/* Ticket Number */}
+            <div className="flex justify-start items-center">
+              <div className="text-xl font-bold text-gray-400 ">
+                {ticketNumber}
+              </div>
             </div>
-          ) : (
-            <div className="mb-2 flex justify-center items-center w-48 h-36 rounded-lg bg-gray-200 border border-gray-300">
-              <ImageIcon size={48} color="#6B7280" />
-            </div>
-          )}
-          <div className="mb-4 flex justify-between w-full px-4">
-            <div className="flex items-center">
-              <FaArrowUp
-                className="text-gray-600 mr-2 cursor-pointer transform transition-transform hover:scale-110"
-                style={{ color: arrowColor }}
-                onClick={handleArrowClick}
-              />
-              <span className="text-gray-700">{formatNumber(currentArrowCount)}</span>
-            </div>
-            <div
-              className="flex items-center cursor-pointer transform transition-transform hover:scale-105"
-              onClick={toggleComments}
-            >
-              <FaComment
-                className="text-gray-600 mr-2"
-                style={{ color: commentColor }}
-              />
-              <span className="text-gray-700">{formatNumber(currentCommentCount)}</span>
-            </div>
-            <div className="flex items-center">
-              <FaEye
-                className="text-gray-600 mr-2 cursor-pointer transform transition-transform hover:scale-110"
-                style={{ color: eyeColor }}
-                onClick={handleEyeClick}
-              />
-              <span className="text-gray-700">{formatNumber(currentViewCount)}</span>
-            </div>
-          </div>
 
-          <div className="flex flex-col text-center items-center mb-4">
-            <h3 className="font-bold text-black text-lg">Address</h3>
-            <p className="text-gray-700">{addressParts.join(", ")}</p>
-          </div>
+            {/* Title */}
+            <div className="flex justify-end items-center ">
+              <div className="font-bold text-2xl pb-4">{title}</div>
+            </div>
 
-          <div className="mt-2 flex justify-center gap-2">
-            <button
-              className="bg-gray-200 text-gray-700 rounded-lg px-2 py-1 hover:bg-gray-300"
-              onClick={onClose}
-            >
-              Back
-            </button>
+            {/* Date opened */}
+            <div className="flex justify-end items-center  text-gray-400">
+              <div className="font-bold text-lg pb-4">2 August</div>
+            </div>
           </div>
         </div>
 
-        {/* Right Section (Map Placeholder) */}
-        <div className="relative w-full lg:w-2/3 bg-gray-200 flex items-center justify-center overflow-hidden">
-          <div className="w-full h-full flex items-center justify-center text-gray-500" id="map">
-            <MapComponent longitude={Number(longitude)} latitude={Number(latitude)} zoom={14} containerId="map" style="mapbox://styles/mapbox/streets-v12" />
+        <div className="flex">
+          <div className="relative w-full lg:w-1/3 p-2 flex flex-col items-center">
+
+            {/*Status*/}
+            <div
+              className={`${color} bg-opacity-75 text-black font-bold text-lg  text-center rounded-lg px-3 py-1 mt-1 w-full`}
+            >
+              {/* {state} */}
+              {"In Progress"}
+            </div>
+
+            {/* ETA */}
+            <div className="flex justify-between mt-4 w-3/4">
+              <div className="text-xl font-bold text-gray-500">
+                Estimated Time:{" "}
+              </div>
+              <div className="text-xl font-bold text-gray-500"> 18 hours</div>
+            </div>
+
+            {/* Discription */}
+            <div className="mb-2 text-center h-[4rem]">
+              <h3 className="font-bold text-black text-lg">Description</h3>
+              <p className="text-gray-700">{description}</p>
+            </div>
+
+            {/* Map */}
+            <div
+              className="w-full h-full flex items-center justify-center text-gray-500"
+              id="map"
+            >
+              <MapComponent
+                longitude={Number(longitude)}
+                latitude={Number(latitude)}
+                zoom={14}
+                containerId="map"
+                style="mapbox://styles/mapbox/streets-v12"
+              />
+            </div>
+
+            {/* Actions */}
+            <div className="mb-4 flex justify-between w-full px-4">
+              {/* Upvotes */}
+              <div className="flex items-center">
+                <FaArrowUp
+                  className="text-gray-600 mr-2 cursor-pointer transform transition-transform hover:scale-110"
+                  style={{ color: arrowColor }}
+                  onClick={handleArrowClick}
+                />
+                <span className="text-gray-700">
+                  {formatNumber(currentArrowCount)}
+                </span>
+              </div>
+
+              {/* Comments */}
+              <div
+                className="flex items-center cursor-pointer transform transition-transform hover:scale-105"
+                onClick={toggleComments}
+              >
+                <FaComment
+                  className="text-gray-600 mr-2"
+                  style={{ color: commentColor }}
+                />
+                <span className="text-gray-700">
+                  {formatNumber(currentCommentCount)}
+                </span>
+              </div>
+
+              {/* Watchlist */}
+              <div className="flex items-center">
+                <FaEye
+                  className="text-gray-600 mr-2 cursor-pointer transform transition-transform hover:scale-110"
+                  style={{ color: eyeColor }}
+                  onClick={handleEyeClick}
+                />
+                <span className="text-gray-700">
+                  {formatNumber(currentViewCount)}
+                </span>
+              </div>
+            </div>
+
+            {/* Address */}
+            <div className="flex justify-between mt-4 w-full">
+              <div className="text-xl font-bold text-gray-500 pr-2">Address: </div>
+              <div className="text-sm font-bold text-gray-500">{address}</div>
+            </div>
+
+            <div className="mt-2 flex justify-center gap-2">
+              <button
+                className="bg-gray-200 text-gray-700 rounded-lg px-2 py-1 hover:bg-gray-300"
+                onClick={onClose}
+              >
+                Back
+              </button>
+            </div>
           </div>
 
-          {/* Move the Directions Button to the Bottom Left */}
-          <div className="absolute bottom-2 left-2 z-10">
-            <Button className="min-w-fit h-fit p-2 bg-white rounded-3xl" onClick={showDirections}>
-              <MapPin size={23} />
-            </Button>
-          </div>
+          {/* Right Section (Map Placeholder) */}
+          <div className="relative w-full lg:w-2/3 bg-gray-200 flex items-center justify-center overflow-hidden">
+            {image && !imageError ? (
+              <div className="mb-2 flex justify-center">
+                <img
+                  src={image}
+                  alt="Fault"
+                  className="rounded-lg w-512 object-cover"
+                  onError={() => setImageError(true)} // Set error state if image fails to load
+                />
+              </div>
+            ) : (
+              <div className="mb-2 flex justify-center items-center w-48 h-36 rounded-lg bg-gray-200 border border-gray-300">
+                <ImageIcon size={48} color="#6B7280" />
+              </div>
+            )}
 
-          {/* Comments Section with Slide Animation */}
-          <div
-            className={`absolute top-0 left-0 w-full h-full bg-white z-20 transform transition-transform duration-300 ${showComments ? "translate-x-0" : "translate-x-full"
+            {/* Move the Directions Button to the Bottom Left */}
+            <div className="absolute bottom-2 left-2 z-10">
+              <Button
+                className="min-w-fit h-fit p-2 bg-white rounded-3xl"
+                onClick={showDirections}
+              >
+                <MapPin size={23} />
+              </Button>
+            </div>
+
+            {/* Comments Section with Slide Animation */}
+            <div
+              className={`absolute top-0 left-0 w-full h-full bg-white z-20 transform transition-transform duration-300 ${
+                showComments ? "translate-x-0" : "translate-x-full"
               }`}
-            style={{ pointerEvents: showComments ? "auto" : "none" }}
-          >
-            <Comments onBack={toggleComments} isCitizen={false} ticketId={ticketId} />
+              style={{ pointerEvents: showComments ? "auto" : "none" }}
+            >
+              <Comments
+                onBack={toggleComments}
+                isCitizen={false}
+                ticketId={ticketId}
+              />
+            </div>
           </div>
         </div>
       </div>
