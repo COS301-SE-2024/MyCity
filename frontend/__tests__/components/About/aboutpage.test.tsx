1784150d50da61e7bc824ae9096556453ff95704
Dominique-Da-Silva <u21629944@tuks.co.za>
2024-09-08 13:24:03 +0200

    Minor Test
    Small frontend test to see what is going on in environment and setup.

---
+++ frontend/__tests__/components/About/aboutpage.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import About from '@/app/about/page';
+import '@testing-library/jest-dom';
+
+describe('About Page', () => {
+  // Test "About Us" heading
+  test('should display "About Us" heading', () => {
+    render(<About />);
+    const aboutUsHeading = screen.getByRole('heading', { name: /about us/i });
+    expect(aboutUsHeading).toBeInTheDocument();
+  });
+});
