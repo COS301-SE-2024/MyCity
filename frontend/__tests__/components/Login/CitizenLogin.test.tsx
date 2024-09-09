f103e314afe39e3e08fd90e6e6574273ff7c237b
Dominique-Da-Silva <u21629944@tuks.co.za>
2024-09-09 08:55:32 +0200

    CitizenLogin test added for updating email
    Uncovered lines added tests (doing incrementally)

--- frontend/__tests__/components/Login/CitizenLogin.test.tsx
+++ frontend/__tests__/components/Login/CitizenLogin.test.tsx
@@ -3,6 +3,7 @@ import { fireEvent, render, screen } from "@testing-library/react";
 
 describe("CitizenLogin", () => {
 
+    /* Test 1: Renders email input correctly */
     it("renders an email input", () => {
         render(<CitizenLogin />);
         // Use getByPlaceholderText if the placeholder is unique
@@ -12,6 +13,7 @@ describe("CitizenLogin", () => {
         expect(emailInput).toHaveAttribute("type", "email");
     });
 
+    /* Test 2: Renders password input correctly */
     it("renders a password input", () => {
         render(<CitizenLogin />);
         const passwordInputs = screen.getAllByLabelText(/Password/);
@@ -22,13 +24,14 @@ describe("CitizenLogin", () => {
     });
 
 
-    /*it("renders a forgot password link", () => {
+    /* Test 3: Renders Forgot Password link */
+    it("renders a 'Forgot password?' link", () => {
         render(<CitizenLogin />);
         const forgotPasswordLink = screen.getByText("Forgot password?");
-
         expect(forgotPasswordLink).toBeInTheDocument();
-    });*/
+    });
 
+    /* Test 4: Renders Login button correctly */
     it("renders a Login button", () => {
         render(<CitizenLogin />);
         const submitButtons = screen.getAllByTestId("submit-btn")
@@ -36,7 +39,7 @@ describe("CitizenLogin", () => {
         expect(submitButton).toBeInTheDocument();
     });
 
-
+     /* Test 5: Calls handleSignIn on form submission */
     test("handler function is called after clicking submit button", () => {
         render(<CitizenLogin />);
         const mockFunction = jest.fn();
@@ -47,6 +50,24 @@ describe("CitizenLogin", () => {
 
         fireEvent.submit(loginForm)
         expect(mockFunction).toHaveBeenCalledTimes(1);
+    });
+
+    /* Test 6: Toggles password visibility */
+    test("toggles password visibility", () => {
+        render(<CitizenLogin />);
+    });
+
+    /* Test 7: Calls handleInputChange and updates email input */
+    it("updates email input correctly", () => {
+        render(<CitizenLogin />);
+        const emailInputs = screen.getAllByPlaceholderText("example@mail.com");
+        const emailInput = emailInputs[0];
+
+        // Simulate typing in the email field
+        fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+
+        // Check if the value has been updated
+        expect(emailInput).toHaveValue("test@example.com");
     });
 
 });
