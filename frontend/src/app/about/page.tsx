ac02dd48f7bfd6664004f089f06f660941dc71a2
Kyle Marshall (u22570285) <u22570285@tuks.co.za>
2024-09-25 08:55:39 +0200

    Guest Mobile fixes

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -225,252 +225,230 @@ export default function About() {
       </div>
 
       {/* Mobile View */}
-      <div className="block sm:hidden">
-        <Navbar showLogin={true} />
+<div className="block sm:hidden">
+  <Navbar showLogin={true} />
 
-        
+  {/* Background Image */}
+  <div
+    style={{
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      backgroundImage:
+        'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/Johannesburg-Skyline.webp")',
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      backgroundAttachment: "fixed",
+      zIndex: -11,
+    }}
+  ></div>
 
-        {/* Background Image */}
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            backgroundImage:
-              'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/Johannesburg-Skyline.webp")',
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            backgroundAttachment: "fixed",
-            zIndex: -11,
-          }}
-        ></div>
+  <main className="relative z-[-10] p-4 mt-0 mb-7">
+    <div className="text-white font-bold transform hover:scale-105 transition-transform duration-200 flex justify-center">
+      <img
+        src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/MyCity-Logo-256.webp"
+        alt="MyCity"
+        width={180}
+        height={180}
+      />
+    </div>
+    
+    {/* Page Title */}
+    <h1 className="text-3xl font-bold text-white mb-6 text-center">
+      About Us
+    </h1>
 
-        <main className="relative z-[-10] p-4 mt-0 mb-7">
-        <div className="text-white font-bold transform hover:scale-105 transition-transform duration-200 flex justify-center">
+    {/* What is MyCity Section */}
+    <section className="mb-10">
+      <h2 className="text-2xl font-bold text-white text-center mb-4">
+        What is MyCity?
+      </h2>
+      <img
+        src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/about_1.webp"
+        alt="MyCity Vision"
+        className="w-full h-auto rounded-lg mb-4"
+      />
+      <p className="text-white text-sm mb-4">
+        MyCity is a Progressive Web Application (PWA) that seeks to redefine the relationship between citizens, municipalities, and service providers by creating a centralized platform for reporting and tracking municipal faults.
+      </p>
+      <p className="text-white text-sm mb-4">
+        The platform simplifies the process of logging issues like broken streetlights or potholes, providing real-time tracking of reports. It helps municipalities to streamline the process by eliminating duplicate reports and making data-driven decisions.
+      </p>
+    </section>
+
+    {/* Meet The Team Section */}
+    <section className="mb-10">
+      <h2 className="text-2xl font-bold text-white text-center mb-4">
+        Meet The Team
+      </h2>
+      <div className="grid grid-cols-1 gap-6">
+        {/* Dominique */}
+        <div className="flex flex-col items-center text-center">
           <img
-            src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/MyCity-Logo-256.webp"
-            alt="MyCity"
-            width={180}
-            height={180}
+            src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/profile_Dominique_Da_Silva.webp"
+            alt="Dominique Da Silva"
+            className="w-32 h-32 rounded-full mb-2"
           />
+          <h3 className="text-lg font-bold text-white">Dominique Da Silva</h3>
+          <p className="text-white text-sm mb-2">Team Leader | Documentation</p>
+          <div className="flex gap-4">
+            <a href="https://github.com/Dominique-Da-Silva">
+              <img
+                src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_github.webp"
+                alt="GitHub"
+                className="w-6"
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/DominiqueDaSilva/">
+              <img
+                src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_linkedin.webp"
+                alt="LinkedIn"
+                className="w-6"
+              />
+            </a>
+          </div>
         </div>
-          {/* Page Title */}
-          <h1 className="text-3xl font-bold text-white mb-6 text-center">
-            About Us
-          </h1>
 
-          {/* What is MyCity Section */}
-          <section className="mb-10">
-            <h2 className="text-2xl font-bold text-white text-center mb-4">
-              What is MyCity?
-            </h2>
-            <img
-              src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/about_1.webp"
-              alt="MyCity Vision"
-              className="w-full h-auto rounded-lg mb-4"
-            />
-            <p className="text-white text-sm mb-4">
-              MyCity is a Progressive Web Application (PWA) that seeks to
-              redefine the relationship between citizens, municipalities, and
-              service providers by creating a centralized platform for reporting
-              and tracking municipal faults.
-            </p>
-            <p className="text-white text-sm mb-4">
-              The platform simplifies the process of logging issues like broken
-              streetlights or potholes, providing real-time tracking of reports.
-              It helps municipalities to streamline the process by eliminating
-              duplicate reports and making data-driven decisions.
-            </p>
-          </section>
+        {/* Kyle */}
+        <div className="flex flex-col items-center text-center">
+          <img
+            src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/profile_Kyle_Marshall.webp"
+            alt="Kyle Marshall"
+            className="w-32 h-32 rounded-full mb-2"
+          />
+          <h3 className="text-lg font-bold text-white">Kyle Marshall</h3>
+          <p className="text-white text-sm mb-2">UI Engineer | Documentation</p>
+          <div className="flex gap-4">
+            <a href="https://github.com/KyleMarshall23">
+              <img
+                src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_github.webp"
+                alt="GitHub"
+                className="w-6"
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/kyle-marshall23/">
+              <img
+                src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_linkedin.webp"
+                alt="LinkedIn"
+                className="w-6"
+              />
+            </a>
+          </div>
+        </div>
 
-          {/* Real-Time Statistics and Data Transparency Section */}
-          <section className="mb-10">
-            <h2 className="text-2xl font-bold text-white text-center mb-4">
-              Real-Time Statistics and Data Transparency
-            </h2>
-            <img
-              src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/about_2.webp"
-              alt="Real-Time Statistics"
-              className="w-full h-auto rounded-lg mb-4"
-            />
-            <p className="text-white text-sm mb-4">
-              MyCity provides real-time statistics, giving visibility into the
-              efficiency of municipal fault management. Citizens can see average
-              resolution times for different types of issues, fostering
-              transparency and trust.
-            </p>
-            <p className="text-white text-sm mb-4">
-              Municipalities and service providers use this data to improve
-              performance and prioritize urgent issues, making better resource
-              allocation decisions.
-            </p>
-          </section>
+        {/* Tino */}
+        <div className="flex flex-col items-center text-center">
+          <img
+            src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/profile_Tino_Gwanyanya.webp"
+            alt="Tino Gwanyanya"
+            className="w-32 h-32 rounded-full mb-2"
+          />
+          <h3 className="text-lg font-bold text-white">Tino Gwanyanya</h3>
+          <p className="text-white text-sm mb-2">
+            Backend Engineer | Integration Engineer
+          </p>
+          <div className="flex gap-4">
+            <a href="https://github.com/Tinogwanz">
+              <img
+                src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_github.webp"
+                alt="GitHub"
+                className="w-6"
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/anold-tinotenda-gwanyanya/">
+              <img
+                src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_linkedin.webp"
+                alt="LinkedIn"
+                className="w-6"
+              />
+            </a>
+          </div>
+        </div>
 
-          {/* Vision for the Future Section */}
-          <section className="mb-10">
-            <h2 className="text-2xl font-bold text-white text-center mb-4">
-              Vision for the Future
-            </h2>
-            <img
-              src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/about_3.webp"
-              alt="Vision for the Future"
-              className="w-full h-auto rounded-lg mb-4"
-            />
-            <p className="text-white text-sm mb-4">
-              MyCity aims to evolve into a comprehensive civic engagement
-              platform that helps municipalities anticipate infrastructure
-              failures before they occur through predictive analytics.
-            </p>
-            <p className="text-white text-sm mb-4">
-              Our goal is to create smarter cities where public services are
-              more responsive and proactive, making MyCity a key tool for
-              municipalities worldwide.
-            </p>
-          </section>
+        {/* Matthew */}
+        <div className="flex flex-col items-center text-center">
+          <img
+            src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/profile_Matthew_Els.webp"
+            alt="Matthew Els"
+            className="w-32 h-32 rounded-full mb-2"
+          />
+          <h3 className="text-lg font-bold text-white">Matthew Els</h3>
+          <p className="text-white text-sm mb-2">DevOps Engineer | Front-End Engineer | Data Engineer</p>
+          <div className="flex gap-4">
+            <a href="https://github.com/MatthewEls">
+              <img
+                src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_github.webp"
+                alt="GitHub"
+                className="w-6"
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/matthewels/">
+              <img
+                src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_linkedin.webp"
+                alt="LinkedIn"
+                className="w-6"
+              />
+            </a>
+          </div>
+        </div>
 
-          {/* Meet The Team Section */}
-          <section className="mb-10">
-            <h2 className="text-2xl font-bold text-white text-center mb-4">
-              Meet The Team
-            </h2>
-            <div className="grid grid-cols-1 gap-6">
-              {/* Dominique */}
-              <div className="flex flex-col items-center text-center">
-                <img
-                  src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/profile_Dominique_Da_Silva.webp"
-                  alt="Dominique Da Silva"
-                  className="w-32 h-32 rounded-full mb-2"
-                />
-                <h3 className="text-lg font-bold text-white">
-                  Dominique Da Silva
-                </h3>
-                <p className="text-white text-sm mb-2">
-                  Team Leader | Documentation
-                </p>
-                <div className="flex gap-4">
-                  <a href="https://github.com/Dominique-Da-Silva">
-                    <img
-                      src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_github.webp"
-                      alt="GitHub"
-                      className="w-6"
-                    />
-                  </a>
-                  <a href="https://www.linkedin.com/in/DominiqueDaSilva/">
-                    <img
-                      src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_linkedin.webp"
-                      alt="LinkedIn"
-                      className="w-6"
-                    />
-                  </a>
-                </div>
-              </div>
-              {/* Kyle */}
-              <div className="flex flex-col items-center text-center">
-                <img
-                  src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/profile_Kyle_Marshall.webp"
-                  alt="Kyle Marshall"
-                  className="w-32 h-32 rounded-full mb-2"
-                />
-                <h3 className="text-lg font-bold text-white">Kyle Marshall</h3>
-                <p className="text-white text-sm mb-2">
-                  UI Engineer | Documentation
-                </p>
-                <div className="flex gap-4">
-                  <a href="https://github.com/KyleMarshall23">
-                    <img
-                      src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_github.webp"
-                      alt="GitHub"
-                      className="w-6"
-                    />
-                  </a>
-                  <a href="https://www.linkedin.com/in/kyle-marshall23/">
-                    <img
-                      src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_linkedin.webp"
-                      alt="LinkedIn"
-                      className="w-6"
-                    />
-                  </a>
-                </div>
-              </div>
-              {/* Tino */}
-              <div className="flex flex-col items-center text-center">
-                <img
-                  src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/profile_Tino_Gwanyanya.webp"
-                  alt="Tino Gwanyanya"
-                  className="w-32 h-32 rounded-full mb-2"
-                />
-                <h3 className="text-lg font-bold text-white">Tino Gwanyanya</h3>
-                <p className="text-white text-sm mb-2">
-                  Backend Engineer | Integration Engineer
-                </p>
-                <div className="flex gap-4">
-                  <a href="https://github.com/Tinogwanz">
-                    <img
-                      src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_github.webp"
-                      alt="GitHub"
-                      className="w-6"
-                    />
-                  </a>
-                  <a href="https://www.linkedin.com/in/anold-tinotenda-gwanyanya/">
-                    <img
-                      src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_linkedin.webp"
-                      alt="LinkedIn"
-                      className="w-6"
-                    />
-                  </a>
-                </div>
-              </div>
-              {/* Other team members can be added in the same format */}
-            </div>
-          </section>
+        {/* Andinda */}
+        <div className="flex flex-col items-center text-center">
+          <img
+            src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/profile_Andinda_Bakainaga.webp"
+            alt="Andinda Bakainaga"
+            className="w-32 h-32 rounded-full mb-2"
+          />
+          <h3 className="text-lg font-bold text-white">Andinda Bakainaga</h3>
+          <p className="text-white text-sm mb-2">Backend Engineer | Documentation</p>
+          <div className="flex gap-4">
+            <a href="https://github.com/Dindosss">
+              <img
+                src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_github.webp"
+                alt="GitHub"
+                className="w-6"
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/andinda-bakainaga-b8b011216/">
+              <img
+                src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_linkedin.webp"
+                alt="LinkedIn"
+                className="w-6"
+              />
+            </a>
+          </div>
+        </div>
+        {/* In Association With Section */}
+<section className="mb-10">
+  <h2 className="text-2xl font-bold text-white text-center mb-4">
+    In Association With
+  </h2>
+  <div className="flex justify-center gap-6">
+    <a href="https://www.epiuselabs.com/">
+      <img
+        src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_epi-use.webp"
+        alt="Epi-Use"
+        className="w-24 h-auto"
+      />
+    </a>
+    <a href="https://www.groupelephant.com/">
+      <img
+        src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_group-elephant.webp"
+        alt="Group Elephant"
+        className="w-24 h-auto"
+      />
+    </a>
+  </div>
+</section>
 
-          {/* Sponsorship Section */}
-          <section className="mb-10">
-            <h2 className="text-2xl font-bold text-white text-center mb-4">
-              Sponsorship
-            </h2>
-            <div className="text-center">
-              <a href="https://placekit.io/">
-                <img
-                  src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_placekit.webp"
-                  alt="PlaceKit"
-                  className="w-32 h-auto mx-auto mb-4"
-                />
-              </a>
-              <p className="text-white text-sm">
-                We are incredibly thankful to PlaceKit for their generous
-                sponsorship and unwavering support for the MyCity project.
-              </p>
-            </div>
-          </section>
-
-          {/* In Association With Section */}
-          <section className="mb-10">
-            <h2 className="text-2xl font-bold text-white text-center mb-4">
-              In Association With
-            </h2>
-            <div className="flex justify-center gap-6">
-              <a href="https://www.epiuselabs.com/">
-                <img
-                  src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_epi-use.webp"
-                  alt="Epi-Use"
-                  className="w-24 h-auto"
-                />
-              </a>
-              <a href="https://www.groupelephant.com/">
-                <img
-                  src="https://mycity-storage-bucket.s3.eu-west-1.amazonaws.com/resources/logo_group-elephant.webp"
-                  alt="Group Elephant"
-                  className="w-24 h-auto"
-                />
-              </a>
-            </div>
-          </section>
-        </main>
       </div>
+    </section>
+  </main>
+</div>
+
     </div>
   );
 }
