d4b60fc119a3f9cf59ddf2685b6305d186299a08
Kyle Marshall (u22570285) <u22570285@tuks.co.za>
2024-09-26 17:49:47 +0200

    Citizen desktop fixes

--- frontend/src/app/create-ticket/citizen/page.tsx
+++ frontend/src/app/create-ticket/citizen/page.tsx
@@ -107,7 +107,7 @@ export default function CreateTicket() {
   <main className="relative flex flex-col items-center justify-start py-5">
     {/* Centered Heading */}
     <h1 className="absolute top-0 left-1/2 transform -translate-x-1/2 text-4xl font-bold mb-3 text-white text-opacity-80 text-center mt-2">
-      Report an Issue
+      Report a Fault
     </h1>
     <div className="w-full max-w-7xl px-2 mt-16">
       <div className="mt-10">
