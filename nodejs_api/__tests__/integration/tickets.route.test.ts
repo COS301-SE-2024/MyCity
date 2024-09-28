70826d8636d2a9cf6fbf74e97951825c641369cc
Tino <132175332+Tinogwanz@users.noreply.github.com>
2024-09-28 19:13:00 +0200

    fixed mistake of swapping unit and integration test fiel locations

--- nodejs_api/__tests__/integration/tickets.route.test.ts
+++ nodejs_api/__tests__/integration/tickets.route.test.ts
@@ -5,6 +5,8 @@ describe("Integration Test - /tickets/fault-types", () => {
     test("GET /tickets/fault-types should return fault types", async () => {
         const response = await request(app).get("/tickets/fault-types");
         expect(response.statusCode).toBe(200);
+
+        // expect the response to be an array of objects of the following shape {asset_id: any; assetIcon: any; multiplier: any;}
         expect(response.body).toEqual(
             expect.arrayContaining([
                 expect.objectContaining({
