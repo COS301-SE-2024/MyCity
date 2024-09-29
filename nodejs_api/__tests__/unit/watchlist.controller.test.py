15086ca55490190eb4044c3192d3cc35bf1970af
Dominique-Da-Silva <u21629944@tuks.co.za>
2024-09-29 16:18:43 +0200

    watchlist controller unit tests done

--- nodejs_api/__tests__/unit/watchlist.controller.test.py
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-import * as watchlistController from "../../src/controllers/watchlist.controller";
-import * as watchlistService from "../../src/services/watchlist.service";
-
-jest.mock("../../src/services/watchlist.service");
