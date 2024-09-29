35b884056f57a3aae9e899e4de3607336e80b8fe
Dominique-Da-Silva <u21629944@tuks.co.za>
2024-09-29 14:36:20 +0200

    close ticket ticket controller unit test

--- nodejs_api/__tests__/unit/tickets.controller.test.ts
+++ nodejs_api/__tests__/unit/tickets.controller.test.ts
@@ -162,6 +162,8 @@ describe("tickets controller - getFaultTypes", () => {
         });
     });
 
+
+    
     describe("acceptTicket", () => {
         test("should return 400 if required fields are missing", async () => {
             req.body = {}; // Missing ticket_id
@@ -191,6 +193,43 @@ describe("tickets controller - getFaultTypes", () => {
             (ticketsService.acceptTicket as jest.Mock).mockRejectedValue(mockError);
 
             await ticketsController.acceptTicket(req as Request, res as Response);
+
+            expect(res.status).toHaveBeenCalledWith(500);
+            expect(res.json).toHaveBeenCalledWith({ Error: mockError.message });
+        });
+    });
+
+
+
+    describe("closeTicket", () => {
+        test("should return 400 if required fields are missing", async () => {
+            req.body = {}; // Missing ticket_id
+
+            await ticketsController.closeTicket(req as Request, res as Response);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({
+                Error: "Missing parameter(s): ticket_id",
+            });
+        });
+
+        test("should return 200 and close ticket on valid input", async () => {
+            req.body = { ticket_id: "123" };
+            const mockResult = { message: "Ticket closed" };
+            (ticketsService.closeTicket as jest.Mock).mockResolvedValue(mockResult);
+
+            await ticketsController.closeTicket(req as Request, res as Response);
+
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.json).toHaveBeenCalledWith(mockResult);
+        });
+
+        test("should return 500 on internal server error", async () => {
+            req.body = { ticket_id: "123" };
+            const mockError = new Error("Internal server error");
+            (ticketsService.closeTicket as jest.Mock).mockRejectedValue(mockError);
+
+            await ticketsController.closeTicket(req as Request, res as Response);
 
             expect(res.status).toHaveBeenCalledWith(500);
             expect(res.json).toHaveBeenCalledWith({ Error: mockError.message });
