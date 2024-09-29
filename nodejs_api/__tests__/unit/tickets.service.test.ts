7ee592fbcfe4cbc0883d7229e90198ab32dbf11a
Dominique-Da-Silva <u21629944@tuks.co.za>
2024-09-29 15:26:57 +0200

    some service tests

--- nodejs_api/__tests__/unit/tickets.service.test.ts
+++ nodejs_api/__tests__/unit/tickets.service.test.ts
@@ -8,6 +8,8 @@ jest.mock("../../src/config/dynamodb.config");
 jest.mock("../../src/config/s3bucket.config");
 jest.mock("ws")
 
+
+
 describe("tickets service - getFaultTypes", () => {
     test("should return a list of fault types", async () => {
         const assetsMock = [
@@ -78,5 +80,68 @@ describe("tickets service - createTicket", () => {
         (dynamoDBDocumentClient.send as jest.Mock).mockResolvedValueOnce({}); // Mock empty response for asset check
 
         await expect(ticketsService.createTicket(formData, mockFile)).rejects.toThrow("NoItems");
+    });
+});
+
+
+describe("tickets service - addWatchlist", () => {
+    const ticketData = {
+        username: "testUser",
+        ticket_id: "ticket123",
+    };
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test("should add ticket to watchlist", async () => {
+    });
+
+    test("should throw error if ticket is already in watchlist", async () => {
+    });
+
+    test("should throw an error if ticket does not exist", async () => {
+        (dynamoDBDocumentClient.send as jest.Mock).mockResolvedValueOnce({ Items: [] }); // Mock no existing watchlist entry
+        (dynamoDBDocumentClient.send as jest.Mock).mockResolvedValueOnce({}); // Mock ticket not found
+
+        await expect(ticketsService.addWatchlist(ticketData)).rejects.toThrow("TicketDoesntExists");
+    });
+});
+
+
+
+
+describe("tickets service - getTicketDetails", () => {
+    const ticketId = "79176f6b-64ed-4edb-b070-4e4fad5ae641";
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test("should return ticket details for a valid ticket ID", async () => {
+    });
+
+    test("should throw an error if ticket does not exist", async () => {
+        (dynamoDBDocumentClient.send as jest.Mock).mockResolvedValueOnce({}); // Mock no ticket found
+
+        await expect(ticketsService.viewTicketData(ticketId)).rejects.toThrow("Failed to get Ticket data");
+    });
+});
+
+
+describe("tickets service - deleteTicket", () => {
+    const ticketId = "bd9969b3-9794-4aa3-aca1-b24aecbfda53";
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test("should close ticket successfully", async () => {
+    });
+
+    test("should throw an error if ticket does not exist", async () => {
+        (dynamoDBDocumentClient.send as jest.Mock).mockResolvedValueOnce({}); // Mock no ticket found
+
+        await expect(ticketsService.closeTicket(ticketId)).rejects.toThrow("TicketDoesntExist");
     });
 });
